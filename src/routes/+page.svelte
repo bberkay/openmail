da7547999a63a2e31005e7073cb6fa0de7c98195
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2024-07-06 23:50:09 +0300

    send-email-form-design-finished

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import Alert from "$lib/Alert.svelte";
+	import Alert from "$lib/components/Alert.svelte";
+	import GetEmailsForm from "$lib/components/SendEmailForm.svelte";
+	import SendEmailForm from "$lib/components/GetEmailsForm.svelte";
 	import { invoke } from "@tauri-apps/api/core";
 	
 	let emails = "";
@@ -10,16 +12,18 @@
 	}
 </script>
 
-<div class="container">
-	<Alert message="This is a success message" type="success" />
-	<input type="text" placeholder="Enter email" />
-	<button type="submit" on:click={getEmails}>Get Emails</button>
-	<p>{emails}</p>
-</div>
+<Alert message="This is a success message" type="success" />
+<main class="container">
+	<div>
+		<GetEmailsForm />
+		<SendEmailForm />
+	</div>
+</main>
 
 <style>
 	.container{
 		max-height: 100vh;
 		max-width: 100vw;
+		display: flex;
 	}
 </style>
