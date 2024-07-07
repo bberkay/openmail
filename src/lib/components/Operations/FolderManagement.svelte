9e01deae4b14fae55c8fdec346c44b977a210fc6
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2024-07-07 15:57:12 +0300

    folder-management-design-fixed

--- src/lib/components/Operations/FolderManagement.svelte
+++ src/lib/components/Operations/FolderManagement.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
-    function showFolderManagementForm(formId: string): void {
+    function showFolderManagementForm(event: Event): void {
+        const formId = (event.target as HTMLButtonElement).dataset.formTargetId;
         document.querySelector('.folder-management-form.active')!.classList.remove('active');
         document.querySelector(`#${formId}`)!.classList.add('active');
+        document.querySelector('.folder-management-buttons .active')!.classList.remove('active');
+        (event.target as HTMLButtonElement).classList.add('active');
     }
 </script>
 
 <section class="folder-management">
     <div class="folder-management-buttons card">
-        <button on:click={() => showFolderManagementForm("create-folder-form")}>Create Folder</button>
-        <button on:click={() => showFolderManagementForm("rename-folder-form")}>Rename Folder</button>
-        <button on:click={() => showFolderManagementForm("delete-folder-form")}>Delete Folder</button>
-        <button on:click={() => showFolderManagementForm("move-folder-form")}>Move Folder</button>
+        <button class="button-text active" data-form-target-id="create-folder-form" on:click={showFolderManagementForm}>Create Folder</button>
+        <span>/</span>
+        <button class="button-text" data-form-target-id="rename-folder-form" on:click={showFolderManagementForm}>Rename Folder</button>
+        <span>/</span>
+        <button class="button-text" data-form-target-id="delete-folder-form" on:click={showFolderManagementForm}>Delete Folder</button>
+        <span>/</span>
+        <button class="button-text" data-form-target-id="move-folder-form" on:click={showFolderManagementForm}>Move Folder</button>
     </div>
     <div class="card">
         <form class = "folder-management-form active" id="create-folder-form">
@@ -46,10 +52,14 @@
             <button type="submit">Delete</button>
         </form>
         <form class = "folder-management-form" id="move-folder-form">
-            <label for="folder_name">Source Folder</label>
-            <select name="folder_name" id="folder_name" required></select>
-            <label for="destination_folder">Destination Folder</label>
-            <select name="destination_folder" id="destination_folder" required></select>
+            <div class="form-group">
+                <label for="folder_name">Source Folder</label>
+                <select name="folder_name" id="folder_name" required></select>
+            </div>
+            <div class="form-group">
+                <label for="destination_folder">Destination Folder</label>
+                <select name="destination_folder" id="destination_folder" required></select>
+            </div>
             <input type="hidden" name="operation" value="move-folder">
             <button type="submit">Move</button>
         </form>
@@ -62,6 +72,21 @@
 
         &.active{
             display: flex;
+        }
+    }
+
+    .folder-management-buttons{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 0.5rem;
+
+        & :not(.active){
+            opacity: 0.7;
+        }
+
+        & .active + span{
+            opacity: 1;
         }
     }
 </style>
