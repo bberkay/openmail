8b9cc2145c844d6b631c0800c2b7c1d945c92718
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2024-12-09 22:31:35 +0300

    Update +layout.ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -9,48 +9,6 @@ const SERVER_CONNECTION_TIMEOUT = 1000 * 2; // 2 seconds
 const SERVER_CONNECTION_TRY_COUNT = 5;
 
 /**
- * Get the server url from the tauri app and
- * check if the server is running. If it is,
- * then set the server url in the shared store.
- * If not, then wait `SERVER_CONNECTION_TIMEOUT` seconds
- * and try again max `SERVER_CONNECTION_TRY_COUNT` times
- */
-async function initServerUrl(): Promise<void> {
-    if (sharedStore.server) {
-        return;
-    }
-
-    let serverConnectionTryCount = 0;
-
-    const getHelloMessageAndSetUrl = async (url: string) => {
-        try {
-            if (serverConnectionTryCount > SERVER_CONNECTION_TRY_COUNT) {
-                error(500, "There was an error while connecting to the server!");
-            }
-
-            serverConnectionTryCount++;
-            const response: Response = await ApiService.get(url, GetRoutes.HELLO);
-            if (response.success) {
-                sharedStore.server = url;
-            } else {
-                error(500, response.message);
-            }
-            return;
-        } catch {
-            setTimeout(async () => {
-                await getHelloMessageAndSetUrl(url);
-            }, SERVER_CONNECTION_TIMEOUT);
-        }
-    }
-
-    await invoke(TauriCommand.GET_SERVER_URL).then(async (url) => {
-        await getHelloMessageAndSetUrl(url as string);
-    }).catch(async () => {
-        error(500, "Error while getting server url!");
-    })
-}
-
-/**
  * Load the accounts from the server
  * and set them in the shared store
  */
@@ -62,10 +20,55 @@ async function loadAccounts() {
     sharedStore.accounts = response.data;
 }
 
+/**
+ * Get the server url from the tauri app and
+ * check if the server is running. If it is,
+ * then set the server url in the shared store and
+ * load the accounts. If not, then wait
+ * `SERVER_CONNECTION_TIMEOUT` seconds and try
+ * again max `SERVER_CONNECTION_TRY_COUNT` times.
+ */
+async function connectToLocalServer(): Promise<void> {
+    if (sharedStore.server) {
+        return;
+    }
+
+    let serverConnectionTryCount = 0;
+
+    const checkUrlAndLoadAccounts = async (url: string) => {
+        try {
+            if (serverConnectionTryCount > SERVER_CONNECTION_TRY_COUNT) {
+                error(500, "There was an error while connecting to the server!");
+            }
+
+            serverConnectionTryCount++;
+            const response: Response = await ApiService.get(url, GetRoutes.HELLO);
+            if (response.success) {
+                sharedStore.server = url;
+                loadAccounts();
+            } else {
+                error(500, response.message);
+            }
+            return;
+        } catch {
+            setTimeout(async () => {
+                await checkUrlAndLoadAccounts(url);
+            }, SERVER_CONNECTION_TIMEOUT);
+        }
+    }
+
+    await invoke(TauriCommand.GET_SERVER_URL).then(async (url) => {
+        await checkUrlAndLoadAccounts(url as string);
+    }).catch(async () => {
+        error(500, "Error while getting server url!");
+    })
+}
+
+
 export const load: LayoutLoad = async ({}) => {
-    await initServerUrl();
-    await loadAccounts();
+    await connectToLocalServer();
 };
+
 
 // Tauri doesn't have a Node.js server to do proper SSR
 // so we will use adapter-static to prerender the app (SSG)
