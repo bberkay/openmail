21e65d7d9fd016ce69180a2c0476a5393315f32e
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2024-12-09 16:27:22 +0300

    Update index.ts

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,48 +1,67 @@
-export interface Response {
-  success: boolean;
-  message: string;
-  data?: any;
-}
-
 export interface Account {
-  fullname: string;
-  email: string;
+    fullname: string;
+    email_address: string;
 }
 
-export interface Email {
-  uid: string;
-  from: string;
-  to: string;
-  subject: string;
-  body_short?: string; // This or body is optional
-  body?: string; // This or body_short is optional
-  date: string;
-  attachments?: Attachment[];
-  flags: string[];
+export interface EmailSummary {
+    uid: string;
+    sender: string;
+    receiver: string;
+    date: string;
+    subject: string;
+    body_short: string;
+    flags?: string[];
+    attachments?: Attachment[];
+}
+
+export interface EmailWithContent {
+    uid: string;
+    sender: string | [string, string];
+    receiver: string;
+    date: string;
+    subject: string;
+    body: string;
+    cc?: string;
+    bcc?: string;
+    flags?: string[];
+    attachments?: Attachment[];
+    message_id?: string;
+    metadata?: Record<string, string>;
 }
 
 export interface SearchCriteria {
-  senders: string[] | null;
-  receivers: string[] | null;
-  subject: string | null;
-  since: string | null;
-  before: string | null;
-  flags: string[] | null;
-  include: string | null;
-  exclude: string | null;
-  has_attachments: boolean;
+    senders: string[] | null;
+    receivers: string[] | null;
+    cc: string[] | null;
+    bcc: string[] | null;
+    subject: string | null;
+    since: string | null;
+    before: string | null;
+    included_flags: string[] | null;
+    excluded_flags: string[] | null;
+    smaller_than: number | null;
+    larger_than: number | null;
+    include: string | null;
+    exclude: string | null;
+    has_attachments: boolean;
 }
 
 export interface Attachment {
-  cid: string;
-  name: string;
-  data: string;
-  size: string;
-  type: string;
+    name: string;
+    data: string;
+    size: string;
+    type: string;
+    cid: string | null;
 }
 
-export type Inboxes = { email: string, folder: string, emails:Email[], total: number }[];
-export type Folders = { email: string, folders:string[] }[];
+export type Inboxes = {
+    email_address: string;
+    folder: string;
+    emails: EmailSummary[];
+    total: number;
+}[];
+
+export type Folders = { email_address: string; folders: string[] }[];
 
 export interface SharedStore {
     server: string;
@@ -51,6 +70,10 @@ export interface SharedStore {
     folders: Folders;
     selectedAccounts: Account[];
     selectedFolder: string;
-    selectedEmail: Email | null;
+    selectedEmail: EmailWithContent | null;
     currentOffset: number;
+}
+
+export enum TauriCommand {
+    GET_SERVER_URL = "get_server_url",
 }
