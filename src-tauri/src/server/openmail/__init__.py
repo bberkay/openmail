d2a74574a3f8170ad27b01d2d48b56e733337329
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2024-12-12 17:29:41 +0300

    Update __init__.py

--- src-tauri/src/server/openmail/__init__.py
+++ src-tauri/src/server/openmail/__init__.py
@@ -1,10 +1,5 @@
 from .openmail import OpenMail
-from .types import SearchCriteria, EmailSummary, EmailToSend, EmailWithContent
 
 __all__ = [
-    "OpenMail", 
-    "SearchCriteria",
-    "EmailSummary",
-    "EmailToSend",
-    "EmailWithContent"
+    "OpenMail",
 ]
