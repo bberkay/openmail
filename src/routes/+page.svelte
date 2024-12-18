06b2aec9b8248f9e432323a03f21e503e86be3d0
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2024-12-19 00:58:31 +0300

    layout-moved-to-page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,62 @@
     import Loader from "$lib/components/Loader.svelte";
     import Inbox from "$lib/components/Inbox.svelte";
     import Register from "$lib/components/Register.svelte";
+    import Sidebar from "$lib/components/Sidebar.svelte";
     import { sharedStore } from "$lib/stores/shared.svelte";
+    import { ApiService, PostRoutes, type Response } from "$lib/services/ApiService";
 
     let isLoading: boolean = $derived(sharedStore.server === "");
+
+    async function deleteAllAccounts() {
+        const response: Response = await ApiService.post(
+            sharedStore.server,
+            PostRoutes.DELETE_EMAIL_ACCOUNTS,
+			{}
+        );
+
+        if (response.success) {
+            sharedStore.accounts = [];
+        }
+    }
+
+	async function recreateWholeUniverse() {
+        const response = await ApiService.post(
+            sharedStore.server,
+            PostRoutes.REFRESH_WHOLE_UNIVERSE,
+            {}
+        );
+
+        if (response.success) {
+            sharedStore.server = "";
+            sharedStore.accounts = [];
+            sharedStore.mailboxes = [];
+            sharedStore.folders = [];
+            sharedStore.selectedAccounts = [];
+            sharedStore.selectedFolder = "Inbox";
+            sharedStore.selectedEmail = null;
+            sharedStore.currentOffset = 0;
+        }
+    }
+
+    async function resetFileSystem() {
+        const response = await ApiService.post(
+            sharedStore.server,
+            PostRoutes.RESET_FILE_SYSTEM,
+            {}
+        );
+
+        if (response.success) {
+            // Show an alert.
+        }
+    }
 </script>
 
 <!--<Alert message="This is a success message" type="success" />-->
 {#if sharedStore.mailboxes.length > 0}
     <main>
-        <!--<section>
+        <section>
             <Sidebar />
-        </section>-->
+        </section>
         <section>
             <Inbox />
         </section>
@@ -27,8 +72,14 @@
 {/if}
 
 <hr>
-
 <pre>{JSON.stringify(sharedStore, null, 2)}</pre>
+
+<hr>
+<div style="text-align: center;">
+    <button onclick={deleteAllAccounts}>Delete All Accounts</button>
+    <button onclick={resetFileSystem}>Reset File System</button>
+    <button onclick={recreateWholeUniverse}>Recreate whole universe</button>
+</div>
 
 <style>
     main {
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,66 @@
-import type { PageLoad } from "./$types";
+import { invoke } from "@tauri-apps/api/core";
+import { TauriCommand } from "$lib/types";
+import { sharedStore } from "$lib/stores/shared.svelte";
+import { error } from "@sveltejs/kit";
+import type { PageLoad } from './$types';
+import { ApiService, GetRoutes, type Response } from "$lib/services/ApiService";
 
-export const load: PageLoad = async ({}) => {};
+/**
+ * Constants
+ */
+const SERVER_CONNECTION_TRY_SLEEP = 1000 * 2; // 2 seconds
+
+/**
+ * Load the accounts from the server
+ * and set them in the shared store
+ */
+async function loadAccounts() {
+    if(!sharedStore.server)
+        return;
+
+    const response: Response = await ApiService.get(sharedStore.server, GetRoutes.GET_EMAIL_ACCOUNTS);
+    sharedStore.accounts = response.data["connected"];
+    sharedStore.failedAccounts = response.data["failed"];
+}
+
+/**
+ * Get the server url from the tauri app and
+ * check if the server is running. If it is,
+ * then set the server url in the shared store and
+ * load the accounts. If not, then wait
+ * `SERVER_CONNECTION_TIMEOUT` seconds and try
+ * again max `SERVER_CONNECTION_TRY_COUNT` times.
+ */
+async function connectToLocalServer(): Promise<void> {
+    if (sharedStore.server) {
+        return;
+    }
+
+    const checkUrlAndLoadAccounts = async (url: string) => {
+        try {
+            const response: Response = await ApiService.get(url, GetRoutes.HELLO);
+            if (response.success) {
+                sharedStore.server = url;
+                await loadAccounts();
+            } else {
+                error(500, response.message);
+            }
+            return;
+        } catch {
+            setTimeout(async () => {
+                await checkUrlAndLoadAccounts(url);
+            }, SERVER_CONNECTION_TRY_SLEEP);
+        }
+    }
+
+    await invoke(TauriCommand.GET_SERVER_URL).then(async (url) => {
+        await checkUrlAndLoadAccounts(url as string);
+    }).catch(async () => {
+        error(500, "Error while getting server url!");
+    })
+}
+
+
+export const load: PageLoad = async ({}) => {
+    await connectToLocalServer();
+};
