1fe9751d1cd85967d6e9e8ec1842f154fcbed946
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-07 04:19:23 +0300

    Create DeleteFolderForm.svelte

---
+++ src/lib/components/Sidebar/DeleteFolderForm.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    interface Props {
+        handleDeleteFolderForm: (e: Event) => void,
+        closeDeleteFolderForm: () => void,
+        folderName: string
+    }
+
+    let { handleDeleteFolderForm, closeDeleteFolderForm, folderName }: Props = $props();
+</script>
+
+<div class="card absolute">
+    <form onsubmit={handleDeleteFolderForm}>
+        <div class="form-group">
+            <label for="folder-name">Folder Name</label>
+            <input
+                type="text"
+                name="folder_name"
+                id="folder-name"
+                placeholder="My New Folder"
+                value={folderName}
+                disabled
+                required
+            />
+        </div>
+        <div class="form-group">
+            <label for="subfolders">Delete Subfolders</label>
+            <input
+                type="checkbox"
+                name="subfolders"
+                id="subfolders"
+            />
+        </div>
+        <div class="display:flex;justify-content:space-between:align-items:center;">
+            <button type="submit" class="bg-primary">Delete</button>
+            <button type="button" onclick={closeDeleteFolderForm}>Cancel</button>
+        </div>
+    </form>
+</div>
