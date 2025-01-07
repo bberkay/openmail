8b320d6c5610a7dcdf52b7cd0a1280d7e29d1e1d
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-07 04:19:27 +0300

    Create MoveFolderForm.svelte

---
+++ src/lib/components/Sidebar/MoveFolderForm.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import { SharedStore } from "$lib/stores/shared.svelte";
+
+    interface Props {
+        handleMoveFolderForm: (e: Event) => void,
+        closeMoveFolderForm: () => void
+    }
+
+    let { handleMoveFolderForm, closeMoveFolderForm }: Props = $props();
+    let selectedFolder: string = $state("");
+</script>
+
+<div class="card absolute">
+    <form onsubmit={handleMoveFolderForm}>
+        <div class="form-group">
+            <label for="folder-name">Select Folder</label>
+            <div class="input-group">
+                <select name="folder_name" id="folder-name" bind:value={selectedFolder}>
+                    <option value="" selected>Select Folder</option>
+                    {#each SharedStore.folders[0].result as folder}
+                        <option value={folder}>{folder}</option>
+                    {/each}
+                </select>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="destination-folder">Destination Folder</label>
+            <div class="input-group">
+                <select name="destination_folder" id="destination-folder">
+                    <option value="" selected>Select Destination Folder</option>
+                    {#each SharedStore.folders[0].result.filter((folder) => folder !== selectedFolder) as folder}
+                        <option value={folder}>{folder}</option>
+                    {/each}
+                </select>
+            </div>
+        </div>
+        <div class="display:flex;justify-content:space-between:align-items:center;">
+            <button type="submit" class="bg-primary">Move</button>
+            <button type="button" onclick={closeMoveFolderForm}>Cancel</button>
+        </div>
+    </form>
+</div>
