a3bd1b0035ead82978fd19ff3d4c79d0e917f5d9
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-07 04:19:30 +0300

    Create RenameFolderForm.svelte

---
+++ src/lib/components/Sidebar/RenameFolderForm.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    interface Props {
+        handleRenameFolderForm: (e: Event) => void,
+        closeRenameFolderForm: () => void,
+        folderName: string
+    }
+
+    let { handleRenameFolderForm, closeRenameFolderForm, folderName }: Props = $props();
+</script>
+
+<div class="card absolute">
+    <form onsubmit={handleRenameFolderForm} id="rename-folder-form">
+        <div class="form-group">
+            <label for="folder-name">Folder Name</label>
+            <input
+                type="text"
+                name="folder_name"
+                id="folder-name"
+                value={folderName}
+                disabled
+                required
+            />
+        </div>
+        <div class="form-group">
+            <label for="new-folder-name">New Folder Name</label>
+            <div class="input-group">
+                <input
+                    type="text"
+                    name="new_folder_name"
+                    id="new-folder-name"
+                    placeholder="New Folder Name"
+                    required
+                />
+            </div>
+        </div>
+        <div class="display:flex;justify-content:space-between:align-items:center;">
+            <button type="submit" class="bg-primary">Rename</button>
+            <button type="button" onclick={closeRenameFolderForm}>Cancel</button>
+        </div>
+    </form>
+</div>
