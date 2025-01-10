da452e348ad223d170b82fadadbc08176589fcea
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-11 02:14:27 +0300

    censor_data feature added

--- src-tauri/src/server/classes/http_request_logger.py
+++ src-tauri/src/server/classes/http_request_logger.py
@@ -13,6 +13,10 @@ MAX_RESPONSE_DATA_LENGTH = 256
 MAX_BYTES_TO_LOG = 1 * 1024 * 1024 # 1 MB
 MAX_BACKUP_COUNT = 5
 
+# Data censoring
+DATA_PREVIEW_LENGTH = 5
+CENSOR_TRACE_LENGTH = 10
+
 class HTTPRequestLogger(logging.Logger):
     def __init__(self):
         super().__init__(APP_NAME)
@@ -27,6 +31,20 @@ class HTTPRequestLogger(logging.Logger):
         file_handler.setFormatter(formatter)
         self.addHandler(stream_handler)
         self.addHandler(file_handler)
+
+    def _censor_data(self, response_data: any) -> str:
+        if not response_data:
+            return ""
+        if isinstance(response_data, dict):
+            return {
+                key: (json.dumps(data)[:DATA_PREVIEW_LENGTH] + "*" * CENSOR_TRACE_LENGTH if data else "")
+                for key, data in response_data.items()
+            }
+        elif isinstance(response_data, list):
+            return "[" + (str(response_data)[1:DATA_PREVIEW_LENGTH] + "*" * CENSOR_TRACE_LENGTH) + "]" if response_data[0] else "[]"
+        elif isinstance(response_data, str):
+            return response_data[:DATA_PREVIEW_LENGTH] + "*" + CENSOR_TRACE_LENGTH
+        return "`response_data` was not censored properly."
 
     def _summarize_data(self, response_data: any) -> any:
         if isinstance(response_data, dict):
@@ -46,6 +64,9 @@ class HTTPRequestLogger(logging.Logger):
     def request(self, request, response):
         try:
             response_data = self._load_data(response._body)
+            if "data" in response_data.keys():
+                response_data["data"] = self._censor_data(response_data["data"])
+
             log_message = (
                 f"{request.method} {request.url} - {response.status_code} - "
                 f"{self._summarize_data(response_data)} - "
