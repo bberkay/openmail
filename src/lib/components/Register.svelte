7a4a2b6ecc6b54fc6b65430796fbd9b159fb8364
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-17 06:01:23 +0300

    Update Register.svelte

--- src/lib/components/Register.svelte
+++ src/lib/components/Register.svelte
@@ -5,104 +5,9 @@
     import type { Account } from "$lib/types";
     import { Folder } from "$lib/types";
     import { ApiService, GetRoutes } from "$lib/services/ApiService";
-    import { AccountController } from "$lib/controllers/AccountController";
-
-    const accountController = new AccountController();
-
-    let currentEditingAccount: Account | null = $state(
-        SharedStore.failedAccounts
-            ? SharedStore.failedAccounts[0]
-            : null
-    );
-
-    $effect(() => {
-        if (!currentEditingAccount && SharedStore.failedAccounts.length > 0)
-            currentEditingAccount = SharedStore.failedAccounts[0];
-    });
-
-    async function addAccount(event: Event) {
-        event.preventDefault();
-        const form = event.target;
-        if (!(form instanceof HTMLFormElement))
-            return;
-
-        const addAccountBtn = document.getElementById('add-account-btn')! as HTMLButtonElement;
-        addAccountBtn.disabled = true;
-        addAccountBtn.textContent = 'Adding Account...';
-
-        const formData = new FormData(form);
-        const response = await accountController.add(
-            formData.get('email_address') as string,
-            formData.get('fullname') as string,
-            formData.get("password") as string
-        );
-
-        if(!response.success){
-            alert(response.message);
-        }
-
-        addAccountBtn.disabled = false;
-        addAccountBtn.textContent = 'Add Account';
-        form.reset();
-    }
-
-    async function editAccount(event: Event) {
-        event.preventDefault();
-        const form = event.target;
-        if (!(form instanceof HTMLFormElement) || !currentEditingAccount)
-            return;
-
-        const editAccountBtn = document.getElementById('edit-account-btn')! as HTMLButtonElement;
-        editAccountBtn.disabled = true;
-        editAccountBtn.textContent = 'Editing Account...';
-
-        const formData = new FormData(form);
-        const response = await accountController.edit(
-            formData.get('email_address') as string,
-            formData.get('fullname') as string,
-            formData.get("password") as string
-        );
-
-        if(response.success){
-            if(SharedStore.failedAccounts.length > 0){
-                currentEditingAccount = SharedStore.failedAccounts[0];
-            }
-        } else {
-            alert(response.message);
-        }
-
-        editAccountBtn.disabled = false;
-        editAccountBtn.textContent = 'Edit Account';
-        form.reset();
-    }
-
-    async function removeAccount(event: Event) {
-        event.preventDefault();
-
-        const button = event.target as HTMLButtonElement;
-        button.textContent = 'Deleting...';
-        button.disabled = true;
-
-        const account = button.getAttribute('data-email-address')!
-        const response = await accountController.remove(account);
-
-        if (!response.success) {
-            alert(response.message);
-        }
-
-        button.disabled = false;
-        button.textContent = 'Delete Account';
-    }
-
-    async function removeAllAccounts() {
-        if(confirm("Are you certain?")){
-            const response = await accountController.removeAll();
-
-            if (!response.success) {
-                alert(response.message);
-            }
-        }
-    }
+    import Form from "$lib/components/Elements/Form.svelte";
+    import AddAccountForm from "./Register/AddAccountForm.svelte";
+    import EditAccountForm from "./Register/EditAccountForm.svelte";
 
     async function getMailboxesOfAllAccounts() {
         const response = await ApiService.get(
@@ -188,57 +93,12 @@
     }
 </script>
 
-{#if currentEditingAccount}
-    <h3>Updating Accounts <button onclick={removeAllAccounts}>Remove All</button></h3>
-    <small>There were {SharedStore.failedAccounts.length} accounts that failed to connect.</small>
-    <ul>
-        {#each SharedStore.failedAccounts as account}
-            <li>
-                <span style="margin-right: 5px;">{account.fullname} &lt;{account.email_address}&gt;</span>
-                <button style="margin-right: 5px;" onclick={() => { currentEditingAccount = account }}>Edit</button>
-                <button onclick={removeAccount} data-email-address={account.email_address}>Remove</button>
-            </li>
-        {/each}
-    </ul>
-    <hr>
+{#if SharedStore.failedAccounts.length > 0}
     <h3>Edit Account</h3>
-    <form onsubmit={editAccount}>
-        <div>
-            <label for="email_address">Email Address</label><br>
-            <input type="email" name="email_address" id="email_address" autocomplete="off" placeholder="someone@domain.xyz" value="{currentEditingAccount.email_address}" readonly required>
-        </div>
-        <div>
-            <label for="password">Password</label><br>
-            <!-- svelte-ignore a11y_autofocus -->
-            <input type="password" name="password" id="password" autocomplete="off" autofocus required>
-        </div>
-        <div>
-            <label for="fullname">Fullname (Optional)</label><br>
-            <input type="text" name="fullname" id="fullname" autocomplete="off" placeholder="Fullname" value="{currentEditingAccount.fullname}"><br>
-            <small style="font-style:italic;margin-top:5px;">Enter your fullname to be displayed in the email.</small>
-        </div>
-        <button type="submit" id="edit-account-btn">Edit Account</button>
-        <button onclick={removeAccount} data-email-address={currentEditingAccount.email_address}>Remove</button>
-    </form>
+    <EditAccountForm />
 {:else}
     <h3>Add Account</h3>
-    <form onsubmit={addAccount}>
-        <div>
-            <label for="email_address">Email Address</label><br>
-            <!-- svelte-ignore a11y_autofocus -->
-            <input type="email" name="email_address" id="email_address" autocomplete="off" value="name@example.com" autofocus required>
-        </div>
-        <div>
-            <label for="password">Password</label><br>
-            <input type="password" name="password" id="password" autocomplete="off" required>
-        </div>
-        <div>
-            <label for="fullname">Fullname (Optional)</label><br>
-            <input type="text" name="fullname" id="fullname" autocomplete="off" value="Test 42"><br>
-            <small style="font-style:italic;margin-top:5px;">Enter your fullname to be displayed in the email.</small>
-        </div>
-        <button type="submit" id="add-account-btn">Add Account</button>
-    </form>
+    <AddAccountForm />
 {/if}
 
 {#if SharedStore.accounts && SharedStore.accounts.length > 0}
