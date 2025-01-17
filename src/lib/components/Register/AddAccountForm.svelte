1b2793624c047c1bd3ad63a35e2936d9169722d3
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-17 06:01:13 +0300

    Create AddAccountForm.svelte

---
+++ src/lib/components/Register/AddAccountForm.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { AccountController } from "$lib/controllers/AccountController";
+    import Form from "$lib/components/Elements/Form.svelte";
+
+    const accountController = new AccountController();
+
+    const addAccount = async (form: HTMLFormElement): Promise<void> => {
+        const formData = new FormData(form);
+        const response = await accountController.add(
+            formData.get('email_address') as string,
+            formData.get('fullname') as string,
+            formData.get("password") as string
+        );
+
+        if(!response.success){
+            alert(response.message);
+        }
+    }
+</script>
+
+<Form Inner={AddAccountForm} operation={addAccount} />
+
+{#snippet AddAccountForm()}
+<div>
+    <div>
+        <label for="email_address">Email Address</label><br>
+        <!-- svelte-ignore a11y_autofocus -->
+        <input type="email" name="email_address" id="email_address" autocomplete="off" value="name@example.com" autofocus required>
+    </div>
+    <div>
+        <label for="password">Password</label><br>
+        <input type="password" name="password" id="password" autocomplete="off" required>
+    </div>
+    <div>
+        <label for="fullname">Fullname (Optional)</label><br>
+        <input type="text" name="fullname" id="fullname" autocomplete="off" value="Test 42"><br>
+        <small style="font-style:italic;margin-top:5px;">Enter your fullname to be displayed in the email.</small>
+    </div>
+    <button type="submit" id="add-account-btn">Add Account</button>
+</div>
+{/snippet}
