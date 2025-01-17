f442cbc24ab611e969a86d17baf67c7c9a854ecd
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-17 06:01:44 +0300

    Create MailboxController.ts

---
+++ src/lib/controllers/MailboxController.ts
@@ -0,0 +1,5 @@
+import { SharedStore, SharedStoreKeys } from "$lib/stores/shared.svelte";
+import { ApiService, PostRoutes, type PostResponse } from "$lib/services/ApiService";
+
+export class MailboxController {
+}
