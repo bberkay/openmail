d96a5325853656a83b39d2331282198c228f4e41
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-17 06:01:33 +0300

    Create Form.svelte

---
+++ src/lib/components/Elements/Form.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { mount, unmount } from "svelte";
+    import Loader from "$lib/components/Loader.svelte";
+    import AddAccountForm from "../Register/AddAccountForm.svelte";
+    import type { Snippet } from 'svelte';
+
+    interface Props {
+        Inner: Snippet;
+        operation: (form: HTMLFormElement) => Promise<void>
+    }
+
+    let { Inner, operation }: Props = $props();
+
+    const makeAnApiRequest = async (e: Event) => {
+        e.preventDefault();
+
+        const form = e.target as HTMLFormElement;
+        const eventTrigger = form.querySelector(
+            'button[type="submit"]'
+        ) as HTMLButtonElement;
+
+        if (!eventTrigger)
+            return;
+
+        eventTrigger.disabled = true;
+        const temp = eventTrigger.innerText;
+        eventTrigger.innerText = "";
+        const loader = mount(Loader, { target: eventTrigger });
+
+        await operation(form);
+
+        eventTrigger.disabled = false;
+        eventTrigger.innerText = temp;
+        unmount(loader);
+        form.reset();
+    }
+</script>
+
+<form onsubmit={makeAnApiRequest}>
+    {@render Inner()}
+</form>
