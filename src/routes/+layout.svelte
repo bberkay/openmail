e5feac3407433ff3837100e62a117dd08ca25c1c
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-20 20:52:38 +0300

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
+
 	let { children } = $props();
+
+	onMount(() => {
+        document.body.addEventListener("click", (e: MouseEvent) => {
+            const target = e.target as HTMLInputElement;
+            const dropdowns = document.querySelectorAll(".dropdown");
+            if (!dropdowns.length)
+                return;
+
+            if (!target.closest('.dropdown, .dropdown-toggle') || (target.closest('.dropdown') && !target.closest('select'))) {
+                dropdowns.forEach(dropdown => dropdown.remove());
+            }
+        });
+	});
 </script>
 
-<div class="overlay"></div>
-{@render children()}
+<main>
+    {@render children()}
+</main>
+
+<div id="modal-container"></div>
