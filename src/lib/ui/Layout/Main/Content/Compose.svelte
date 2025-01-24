e43745351007275c39c8ea95efb56fd1e4ea5294
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-24 05:05:51 +0300

    Update Compose.svelte

--- src/lib/ui/Layout/Main/Content/Compose.svelte
+++ src/lib/ui/Layout/Main/Content/Compose.svelte
@@ -1,70 +1,82 @@
 <script lang="ts">
+    import { SharedStore } from '$lib/stores/shared.svelte';
+    import { MailboxController } from "$lib/controllers/MailboxController";
     import { onMount } from 'svelte';
     import { WYSIWYGEditor } from '@bberkay/wysiwygeditor';
-    import { SharedStore } from '$lib/stores/shared.svelte';
-    import { createDomObject, makeSizeHumanReadable } from '$lib/utils';
-    import Select from "$lib/components/Elements/Select.svelte";
-    import Form from "$lib/components/Elements/Form.svelte";
-    import { MailboxController } from "$lib/controllers/MailboxController";
-    import { show as showContent } from "$lib/components/Content.svelte";
-    import Inbox from '$lib/components/Inbox/Inbox.svelte';
-    import Option from "$lib/components/Elements/Option.svelte";
+    import { createDomObject, makeSizeHumanReadable, isEmailValid } from '$lib/utils';
+    import Select from "$lib/ui/Elements/Select";
+    import Form from "$lib/ui/Elements/Form";
+    import { backToDefault } from "$lib/ui/Layout/Main/Content.svelte";
+
+    /* Constants */
 
     const mailboxController = new MailboxController();
-
-    let sender: string = $state(SharedStore.accounts[0].email_address);
-    let body: WYSIWYGEditor;
-    let attachments: File[] | null = null;
-    const fileTagTemplate = `
+    const fileTemplate = `
         <span class="tag">
             <span class="value">{name}</span>
             <button type="button" style="margin-left:5px;">X</button>
         </span>
     `;
+    const tagTemplate = `
+        <span class="tag">
+            <span class="value">{tag}</span>
+            <button type="button" style="margin-left:5px;" onclick="this.parentElement.remove()">X</button>
+        </span>
+    `;
+
+    /* Variables */
+
+    let sender: string = $state(SharedStore.accounts[0].email_address);
+    let body: WYSIWYGEditor;
+    let attachments: File[] | null = null;
 
     onMount(() => {
         body = new WYSIWYGEditor('body');
         body.init();
     });
 
-    const showInbox = () => {
-        showContent(Inbox);
+    /* Compose Form Handling Functions */
+
+    const selectSender = (email_address: string | null) => {
+        if (!email_address) return;
+        sender = email_address;
     }
 
-    const handleTagEnter = (e: KeyboardEvent) => {
+    const addEnteredEmail = (e: KeyboardEvent) => {
         const target = e.target as HTMLInputElement;
-        const tags = target.closest(".form-group")!.querySelector('.tags')! as HTMLElement;
-        const isEmailValid = target.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/);
-        if((e.key === 'Spacebar' || e.key === ' ')){
-            if(target.value !== '' && isEmailValid){
-                tags.style.display = 'flex';
-                tags.innerHTML += `<span class="tag"><span class="value">${target.value}</span><button type="button" style="margin-left:5px;" onclick="this.parentElement.remove()">X</button></span>`;
-                target.value = '';
-            }
-            else{
-                target.style.transform = 'scale(1.02)';
+        const email = target.value;
+        const emails = target
+            .closest(".form-group")!
+            .querySelector(".emails")! as HTMLElement;
+        if (e.key === "Spacebar" || e.key === " ") {
+            if (email !== "" && isEmailValid(email)) {
+                emails.style.display = "flex";
+                emails.innerHTML += tagTemplate.replace("{tag}", email);
+                target.value = "";
+            } else {
+                target.style.transform = "scale(1.02)";
                 setTimeout(() => {
-                    target.style.transform = 'scale(1)';
+                    target.style.transform = "scale(1)";
                 }, 100);
             }
         }
     }
 
-    const handleFileUpload = (e: Event) => {
+    const addFile = (e: Event) => {
         const target = e.target as HTMLInputElement;
         attachments = Array.from(target.files as FileList);
         const tags = target.parentElement!.parentElement!.querySelector('.tags')! as HTMLElement;
         Array.from(attachments).forEach((file, index) => {
             const name = `${file.name} (${makeSizeHumanReadable(file.size)})`;
-            const fileNode = createDomObject(fileTagTemplate.replace('{name}', name));
-            fileNode!.querySelector('button')!.addEventListener('click', (e) => {
+            const fileNode = createDomObject(fileTemplate.replace('{name}', name));
+            fileNode!.querySelector('button')!.addEventListener('click', (e: Event) => {
                 removeFile(e, index);
             });
             tags.appendChild(fileNode!);
         });
     }
 
-    const clearAllFiles = (e: Event) => {
+    const removeAllFiles = (e: Event) => {
         const target = e.target as HTMLInputElement;
         const fileInput = target.closest(".form-group")!.querySelector('input') as HTMLInputElement;
         fileInput.value = '';
@@ -84,11 +96,13 @@
         target.parentElement?.remove();
     }
 
-    const getTagValues = (id: string): string => {
+    function getEmailAddresses(id: string): string {
         return Array.from(
             document.getElementById(id)!.querySelectorAll("span.value")
         ).map(span => span.textContent).join(',');
     }
+
+    /* Main Operation */
 
     const sendEmail = async (e: Event): Promise<void> => {
         const form = e.target as HTMLFormElement;
@@ -96,56 +110,52 @@
 
         formData.set('sender', sender);
         formData.set('body', body.getHTMLContent());
-        formData.set('receiver', getTagValues("saved-receivers"));
-        formData.set('cc', getTagValues("saved-cc"));
-        formData.set('bcc', getTagValues("saved-bcc"));
+        formData.set('receiver', getEmailAddresses("added-receivers"));
+        formData.set('cc', getEmailAddresses("added-cc"));
+        formData.set('bcc', getEmailAddresses("added-bcc"));
 
         const response = await mailboxController.sendEmail(formData);
 
         alert(response.message);
         body.clear();
     }
-
-    const selectSender = (email_address: string | null) => {
-        if (!email_address) return;
-        sender = email_address;
-    }
 </script>
 
 <div class="header">
     <h2>Compose</h2>
-    <button onclick={showInbox}>X</button>
+    <button onclick={backToDefault}>X</button>
 </div>
 
 <Form onsubmit={sendEmail}>
     <div>
         <div class="form-group">
             <label for="sender">Sender</label>
-            <Select onchange={selectSender} value={SharedStore.accounts[0].email_address}>
+            <Select.Menu onchange={selectSender} value={SharedStore.accounts[0].email_address}>
                 {#each SharedStore.accounts as account}
-                    <Option value={account.email_address}>{account.fullname} &lt;{account.email_address}&gt;</Option>
+                    <Select.Option value={account.email_address}>
+                        {account.fullname} &lt;{account.email_address}&gt;
+                    </Select.Option>
                 {/each}
-            </Select>
+            </Select.Menu>
         </div>
         <div class="form-group">
             <label for="receiver">Receiver(s)</label>
-            <input type="email" name="receiver" id="receiver" placeholder="someone@domain.xyz" onkeyup={handleTagEnter} required>
-            <div class="tags" id = "saved-receivers"></div>
+            <input type="email" name="receiver" id="receiver" placeholder="someone@domain.xyz" onkeyup={addEnteredEmail} required>
+            <div class="tags emails" id = "added-receivers"></div>
         </div>
         <div class="form-group">
             <label for="subject">Subject</label>
             <input type="text" name="subject" id="subject" placeholder="Subject" required>
-            <div class="tags"></div>
         </div>
         <div class="form-group">
             <label for="cc">Cc</label>
-            <input type="email" name="cc" id="cc" placeholder="someone@domain.xyz" onkeyup={handleTagEnter}>
-            <div class="tags" id = "saved-cc"></div>
+            <input type="email" name="cc" id="cc" placeholder="someone@domain.xyz" onkeyup={addEnteredEmail}>
+            <div class="tags emails" id = "added-cc"></div>
         </div>
         <div class="form-group">
             <label for="bcc">Bcc</label>
-            <input type="email" name="bcc" id="bcc" placeholder="someone@domain.xyz" onkeyup={handleTagEnter}>
-            <div class="tags" id = "saved-bcc"></div>
+            <input type="email" name="bcc" id="bcc" placeholder="someone@domain.xyz" onkeyup={addEnteredEmail}>
+            <div class="tags emails" id = "added-bcc"></div>
         </div>
         <div class="form-group">
             <label for="body">Body</label>
@@ -154,8 +164,8 @@
         <div class="form-group">
             <label for="attachments">Attachment(s)</label>
             <div class="input-group">
-                <input type="file" name="attachments" id="attachments" onchange={handleFileUpload} multiple>
-                <button type="button" onclick={clearAllFiles}>Clear</button>
+                <input type="file" name="attachments" id="attachments" onchange={addFile} multiple>
+                <button type="button" onclick={removeAllFiles}>Remove All</button>
             </div>
             <div class="tags"></div>
         </div>
