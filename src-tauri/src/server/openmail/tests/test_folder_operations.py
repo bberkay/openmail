3f838dc3edb454ab748bd3251a0ddac1e7f8ce8b
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-25 23:31:07 +0300

    Update test_folder_operations.py

--- src-tauri/src/server/openmail/tests/test_folder_operations.py
+++ src-tauri/src/server/openmail/tests/test_folder_operations.py
@@ -49,6 +49,8 @@ class TestFolderOperations(unittest.TestCase):
             parent_folder_name, folder_name = folder.rsplit("/", 1) if "/" in folder else (None, folder)
             self.__class__._openmail.imap.create_folder(folder_name, parent_folder_name)
 
+        self.__class__._created_test_folders.extend([folder for folder in folder_structure if "/" not in folder])
+
         folders = self.__class__._openmail.imap.get_folders()
 
         # Standard Folders like INBOX, JUNK, TRASH
@@ -60,14 +62,13 @@ class TestFolderOperations(unittest.TestCase):
             folder_structure
         )
 
-        self.__class__._created_test_folders.extend([folder for folder in folder_structure if "/" not in folder])
-
     def test_get_subfolders_operation(self):
         print("test_get_subfolders_operation...")
         new_folder_name, parent_folder_name = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, create_parent=True)
+        self.__class__._created_test_folders.append(parent_folder_name)
+
         subfolders = self.__class__._openmail.imap.get_folders(parent_folder_name)
         self.assertListEqual(subfolders, [f"{parent_folder_name}/{new_folder_name}"])
-        self.__class__._created_test_folders.append(parent_folder_name)
 
     def test_get_folders_as_tagged_operation(self):
         print("test_get_folders_as_tagged_operation...")
@@ -84,13 +85,14 @@ class TestFolderOperations(unittest.TestCase):
         folder_name = NameGenerator.random_folder_name_with_uuid()
 
         status, msg = self.__class__._openmail.imap.create_folder(folder_name)
+
         self.assertTrue(status, msg)
+        self.__class__._created_test_folders.append(folder_name)
+
         self.assertIn(
             folder_name,
             self.__class__._openmail.imap.get_folders()
         )
-
-        self.__class__._created_test_folders.append(folder_name)
 
     def test_create_folder_with_parent_operation(self):
         print("test_create_folder_with_parent_operation...")
@@ -100,15 +102,15 @@ class TestFolderOperations(unittest.TestCase):
         )
 
         status, msg = self.__class__._openmail.imap.create_folder(folder_name, parent_folder_name)
+
         self.assertTrue(status, msg)
+        self.__class__._created_test_folders.append(created_folder_name)
 
         created_folder_name = f"{parent_folder_name}/{folder_name}"
         self.assertIn(
             created_folder_name,
             self.__class__._openmail.imap.get_folders()
         )
-
-        self.__class__._created_test_folders.append(created_folder_name)
 
     def test_create_nonascii_folder_operation(self):
         print("test_create_nonascii_folder_operation...")
@@ -119,7 +121,9 @@ class TestFolderOperations(unittest.TestCase):
         )
 
         status, msg = self.__class__._openmail.imap.create_folder(folder_name, parent_folder_name)
+
         self.assertTrue(status, msg)
+        self.__class__._created_test_folders.append(created_folder_name)
 
         created_folder_name = f"{parent_folder_name}/{folder_name}"
         self.assertIn(
@@ -127,15 +131,16 @@ class TestFolderOperations(unittest.TestCase):
             self.__class__._openmail.imap.get_folders()
         )
 
-        self.__class__._created_test_folders.append(created_folder_name)
-
     def test_move_folder_operation(self):
         print("test_move_folder_operation...")
         folder_name1 = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail)
         folder_name2 = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail)
+        self.__class__._created_test_folders.append(folder_name1)
 
         status, msg = self.__class__._openmail.imap.move_folder(folder_name1, folder_name2)
+
         self.assertTrue(status, msg)
+        self.__class__._created_test_folders.append(folder_name2)
 
         moved_folder_name = f"{folder_name2}/{folder_name1}"
         self.assertIn(
@@ -143,15 +148,16 @@ class TestFolderOperations(unittest.TestCase):
             self.__class__._openmail.imap.get_folders()
         )
 
-        self.__class__._created_test_folders.append(folder_name1, folder_name2)
-
     def test_move_folder_with_subfolders_operation(self):
         print("test_move_folder_with_subfolders_operation...")
         _, parent_folder = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, "openmail-folder-test-inner-", create_parent=True)
         target_folder_name = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, "openmail-folder-test-inner-")
+        self.__class__._created_test_folders.append(parent_folder)
 
         status, msg = self.__class__._openmail.imap.move_folder(parent_folder, target_folder_name)
+
         self.assertTrue(status, msg)
+        self.__class__._created_test_folders.append(target_folder_name)
 
         moved_folder_name = f"{target_folder_name}/{parent_folder}"
         self.assertIn(
@@ -160,15 +166,16 @@ class TestFolderOperations(unittest.TestCase):
         )
         self.assertFalse(len(self.__class__._openmail.imap.get_folders(moved_folder_name)) == 0)
 
-        self.__class__._created_test_folders.append(parent_folder, target_folder_name)
-
     def test_move_child_folder_operation(self):
         print("test_move_child_folder_operation...")
         folder_name, parent_folder = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, "openmail-folder-test-inner-", create_parent=True)
         target_folder_name = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, "openmail-folder-test-inner-")
+        self.__class__._created_test_folders.append(parent_folder)
 
         status, msg = self.__class__._openmail.imap.move_folder(f"{parent_folder}/{folder_name}", target_folder_name)
+
         self.assertTrue(status, msg)
+        self.__class__._created_test_folders.append(target_folder_name)
 
         moved_folder_name = f"{target_folder_name}/{folder_name}"
         self.assertIn(
@@ -176,15 +183,16 @@ class TestFolderOperations(unittest.TestCase):
             self.__class__._openmail.imap.get_folders()
         )
 
-        self.__class__._created_test_folders.append(parent_folder, target_folder_name)
-
     def test_move_child_folder_to_another_parent_folder_operation(self):
         print("test_move_child_folder_to_another_parent_folder_operation...")
         folder_name, parent_folder = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, "openmail-folder-test-inner-", create_parent=True)
         target_folder_name, parent_target_folder_name = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, "openmail-folder-test-inner-", create_parent=True)
+        self.__class__._created_test_folders.append(parent_folder)
 
         status, msg = self.__class__._openmail.imap.move_folder(f"{parent_folder}/{folder_name}", f"{parent_target_folder_name}/{target_folder_name}")
+
         self.assertTrue(status, msg)
+        self.__class__._created_test_folders.append(parent_target_folder_name)
 
         moved_folder_name = f"{parent_target_folder_name}/{target_folder_name}/{folder_name}"
         self.assertIn(
@@ -192,15 +200,16 @@ class TestFolderOperations(unittest.TestCase):
             self.__class__._openmail.imap.get_folders()
         )
 
-        self.__class__._created_test_folders.append(parent_folder, parent_target_folder_name)
-
     def test_move_nonascii_folder_operation(self):
         print("test_move_nonascii_folder_operation...")
         folder_name1 = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, "openmail-folder-test-ü-")
         folder_name2 = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, "openmail-folder-test-ç-")
+        self.__class__._created_test_folders.append(folder_name1)
 
         status, msg = self.__class__._openmail.imap.move_folder(folder_name1, folder_name2)
+
         self.assertTrue(status, msg)
+        self.__class__._created_test_folders.append(folder_name2)
 
         moved_folder_name = f"{folder_name2}/{folder_name1}"
         self.assertIn(
@@ -208,11 +217,11 @@ class TestFolderOperations(unittest.TestCase):
             self.__class__._openmail.imap.get_folders()
         )
 
-        self.__class__._created_test_folders.append(folder_name1, folder_name2)
-
     def test_delete_folder_operation(self):
         print("test_delete_folder_operation...")
         folder_name = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail)
+        self.__class__._created_test_folders.append(folder_name)
+
         status, msg = self.__class__._openmail.imap.delete_folder(folder_name)
         self.assertTrue(status, msg)
         self.assertNotIn(
@@ -220,11 +229,10 @@ class TestFolderOperations(unittest.TestCase):
             self.__class__._openmail.imap.get_folders()
         )
 
-        self.__class__._created_test_folders.append(folder_name)
-
     def test_delete_folder_with_subfolders_operation(self):
         print("test_delete_folder_with_subfolders_operation...")
         _, parent_folder_name = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, create_parent=True)
+        self.__class__._created_test_folders.append(parent_folder_name)
 
         status, msg = self.__class__._openmail.imap.delete_folder(parent_folder_name, True)
         self.assertTrue(status, msg)
@@ -233,29 +241,31 @@ class TestFolderOperations(unittest.TestCase):
             self.__class__._openmail.imap.get_folders()
         )
 
-        self.__class__._created_test_folders.append(parent_folder_name)
-
     def test_rename_folder_operation(self):
         print("test_rename_folder_operation...")
         folder_name = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail)
+        self.__class__._created_test_folders.append(folder_name)
 
         new_folder_name = NameGenerator.random_folder_name_with_uuid()
         status, msg = self.__class__._openmail.imap.rename_folder(folder_name, new_folder_name)
+
         self.assertTrue(status, msg)
+        self.__class__._created_test_folders.append(new_folder_name)
 
         folders = self.__class__._openmail.imap.get_folders()
         self.assertNotIn(folder_name, folders)
         self.assertIn(new_folder_name, folders)
 
-        self.__class__._created_test_folders.append(folder_name, new_folder_name)
-
     def test_rename_child_folder_operation(self):
         print("test_rename_child_folder_operation...")
         folder_name, parent_folder_name = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, create_parent=True)
+        self.__class__._created_test_folders.append(parent_folder_name)
 
         new_folder_name = NameGenerator.random_folder_name_with_uuid()
         status, msg = self.__class__._openmail.imap.rename_folder(f"{parent_folder_name}/{folder_name}", new_folder_name)
+
         self.assertTrue(status, msg)
+        self.__class__._created_test_folders.append(new_folder_name)
 
         folders = self.__class__._openmail.imap.get_folders()
         old_folder_name = f"{parent_folder_name}/{folder_name}"
@@ -263,35 +273,35 @@ class TestFolderOperations(unittest.TestCase):
         self.assertNotIn(old_folder_name, folders)
         self.assertIn(new_folder_name, folders)
 
-        self.__class__._created_test_folders.append(parent_folder_name, new_folder_name)
-
     def test_rename_folder_with_subfolders_operation(self):
         print("test_rename_folder_with_subfolders_operation...")
         _, parent_folder_name = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, create_parent=True)
+        self.__class__._created_test_folders.append(parent_folder_name)
 
         new_parent_folder_name = NameGenerator.random_folder_name_with_uuid()
         status, msg = self.__class__._openmail.imap.rename_folder(parent_folder_name, new_parent_folder_name)
+
         self.assertTrue(status, msg)
+        self.__class__._created_test_folders.append(new_parent_folder_name)
 
         folders = self.__class__._openmail.imap.get_folders()
         self.assertIn(new_parent_folder_name, folders)
         self.assertNotIn(parent_folder_name, folders)
 
-        self.__class__._created_test_folders.append(parent_folder_name, new_parent_folder_name)
-
     def test_rename_nonascii_folder_operation(self):
         print("test_rename_nonascii_folder_operation...")
         folder_name = DummyOperator.create_test_folder_and_get_name(self.__class__._openmail, "openmail-folder-test-ü-")
+        self.__class__._created_test_folders.append(folder_name)
 
         new_folder_name = NameGenerator.random_folder_name_with_uuid("openmail-folder-test-ç-")
         status, msg = self.__class__._openmail.imap.rename_folder(folder_name, new_folder_name)
+
         self.assertTrue(status, msg)
+        self.__class__._created_test_folders.append(new_folder_name)
 
         folders = self.__class__._openmail.imap.get_folders()
         self.assertIn(new_folder_name, folders)
         self.assertNotIn(folder_name, folders)
-
-        self.__class__._created_test_folders.append(folder_name, new_folder_name)
 
     @classmethod
     def cleanup(cls):
