f85c7bd77796d5bfe9d94fab01bd8e0a775854da
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-31 04:46:16 +0300

    Create test_account_manager.py

---
+++ src-tauri/src/server/classes/tests/test_account_manager.py
@@ -0,0 +1,142 @@
+import unittest
+import json
+
+from classes.account_manager import AccountManager, Account, AccountWithPassword
+
+class TestAccountManager(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("Setting up test `TestAccountManager`...")
+        cls.addClassCleanup(cls.cleanup)
+
+        cls._account_manager = AccountManager()
+
+    def test_is_exists(self):
+        pass
+
+    def test_add_account(self):
+        pass
+
+    def test_get_account(self):
+        pass
+
+    def test_get_all_accounts(self):
+        pass
+
+    def test_edit_account(self):
+        pass
+
+    def test_remove_account(self):
+        pass
+
+    def test_remove_all_accounts(self):
+        pass
+
+    def test_add_duplicate(self):
+        pass
+
+    def test_edit_nonexist(self):
+        pass
+
+    def test_clear(self):
+        pass
+
+    def test_destroy(self):
+        pass
+
+    def test_login(self):
+        print("test_login...")
+        failed_accounts = []
+        for credential in self.__class__._credentials:
+            status, message = self.__class__._openmail.connect(
+                credential["email"],
+                credential["password"]
+            )
+
+            if status:
+                print(f"Successfully logged in {credential["email"]}")
+            else:
+                failed_accounts.append({"email": credential["email"], "status": status, "message": message})
+
+            self.__class__._openmail.disconnect()
+
+        if len(failed_accounts) > 0:
+            self.fail(f"There were accounts that could not be logged in: {failed_accounts}")
+
+    def test_login_with_nonascii_email(self):
+        print("test_login_with_nonascii_email...")
+        failed_accounts = []
+        for credential in self.__class__._credentials:
+            if not contains_non_ascii(credential["email"]):
+                print(f"{credential["email"]} does not include nonascii character. Skipping...")
+                continue
+
+            status, message = self.__class__._openmail.connect(
+                credential["email"],
+                credential["password"]
+            )
+
+            if status:
+                print(f"Successfully logged in {credential["email"]}")
+            else:
+                failed_accounts.append({"email": credential["email"], "status": status, "message": message})
+
+            self.__class__._openmail.disconnect()
+
+        if len(failed_accounts) > 0:
+            self.fail(f"There were accounts that could not be logged in: {failed_accounts}")
+
+    def test_login_with_nonascii_password(self):
+        print("test_login_with_nonascii_password...")
+        failed_accounts = []
+        for credential in self.__class__._credentials:
+            if not contains_non_ascii(credential["password"]):
+                print(f"{credential["email"]} does not include nonascii character. Skipping...")
+                continue
+
+            status, message = self.__class__._openmail.connect(
+                credential["email"],
+                credential["password"]
+            )
+
+            if status:
+                print(f"Successfully logged in {credential["email"]}")
+            else:
+                failed_accounts.append({"email": credential["email"], "status": status, "message": message})
+
+            self.__class__._openmail.disconnect()
+
+        if len(failed_accounts) > 0:
+            self.fail(f"There were accounts that could not be logged in: {failed_accounts}")
+
+    def test_logout(self):
+        print("test_logout...")
+        logged_in_failed_accounts = []
+        logged_out_failed_accounts = []
+        for credential in self.__class__._credentials:
+            status, message = self.__class__._openmail.connect(
+                credential["email"],
+                credential["password"]
+            )
+
+            if status:
+                print(f"Successfully logged in {credential["email"]}")
+            else:
+                logged_in_failed_accounts.append({"email": credential["email"], "status": status, "message": message})
+
+            status, message = self.__class__._openmail.disconnect()
+            if status:
+                print(f"Successfully logged out from {credential["email"]}")
+            else:
+                logged_out_failed_accounts.append({"email": credential["email"], "status": status, "message": message})
+
+        if len(logged_in_failed_accounts) > 0:
+            self.fail(f"There were accounts that could not be logged in: {logged_in_failed_accounts}")
+
+        if len(logged_out_failed_accounts) > 0:
+            self.fail(f"There were accounts that could not be logged out: {logged_out_failed_accounts}")
+
+    @classmethod
+    def cleanup(cls):
+        print("Cleaning up test `TestConnectOperations`...")
+        cls._openmail.disconnect()
