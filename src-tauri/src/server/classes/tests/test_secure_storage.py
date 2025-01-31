c2843895e4108c3bc75876bb9524bfac4829aaf5
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-31 04:46:22 +0300

    Update test_secure_storage.py

--- src-tauri/src/server/classes/tests/test_secure_storage.py
+++ src-tauri/src/server/classes/tests/test_secure_storage.py
@@ -1,22 +1,64 @@
 import unittest
 import json
+from typing import cast
 
-from classes.secure_storage import SecureStorage, SecureStorageKey, RSACipher
+from classes.secure_storage import SECURE_STORAGE_KEY_LIST, SecureStorage, SecureStorageKey, RSACipher, SecureStorageKeyValueType
 
 class TestSecureStorage(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        # TODO: Check also cache in every method.
         print("Setting up test `TestSecureStorage`...")
         cls.addClassCleanup(cls.cleanup)
-
         cls._secure_storage = SecureStorage()
-        with open("./credentials.json") as credentials:
-            credentials = json.load(credentials)
+
+        cls._secure_storage.add_key(
+            SecureStorageKey.CompleteBackupBeforeTesting,
+            [
+                cls._secure_storage.get_key_value(
+                    cast(SecureStorageKey, key_name)
+                )
+                for key_name in SECURE_STORAGE_KEY_LIST
+            ],
+            SecureStorageKeyValueType.TestingKey
+        )
 
     def test_add_key(self):
         pass
 
+    def test_get_key_value(self):
+        pass
+
+    def test_update_key(self):
+        pass
+
+    def test_delete_key(self):
+        pass
+
+    def test_clear(self):
+        pass
+
+    def test_destroy(self):
+        pass
+
+    def test_aesgcm_encryption(self):
+        pass
+
+    def test_aesgcm_decryption(self):
+        pass
+
+    def test_aesgcm_rotation(self):
+        pass
+
+    def test_rsa_encryption(self):
+        pass
+
+    def test_rsa_decryption(self):
+        pass
+
+    def test_rsa_rotation(self):
+        pass
+
     @classmethod
     def cleanup(cls):
-        print("Cleaning up test `TestConnectOperations`...")
-        cls._openmail.disconnect()
+        print("Cleaning up test `TestSecureStorage`...")
