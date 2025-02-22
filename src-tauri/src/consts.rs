0e7cc4b8071c0ef67cf12f4d905bc09b8df1605a
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-02-22 18:45:02 +0300

    Update consts.rs

--- src-tauri/src/consts.rs
+++ src-tauri/src/consts.rs
@@ -1,9 +1,4 @@
 pub const IS_WINDOWS: bool = cfg!(target_os = "windows");
-pub const UVICORN_CREATE_VENV_SCRIPT_PATH: &str = if IS_WINDOWS {
-    "./windows/create_venv.bat"
-} else {
-    "./linux/create_venv.sh"
-};
 pub const UVICORN_START_SCRIPT_PATH: &str = if IS_WINDOWS {
     "./windows/start_uvicorn.bat"
 } else {
