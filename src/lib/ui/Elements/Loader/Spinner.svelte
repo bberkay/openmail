0c4008401f66f581b1842bb1bd40611fb85bc313
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-02-23 03:16:21 +0300

    Update Spinner.svelte

--- src/lib/ui/Elements/Loader/Spinner.svelte
+++ src/lib/ui/Elements/Loader/Spinner.svelte
@@ -11,3 +11,27 @@
 <span>
     <small class="loader"></small> {text}
 </span>
+
+<style>
+    .loader {
+        border: 2px solid #2a2a2a;
+        border-radius: 50%;
+        border-top: 2px solid #fff;
+        width: 10px;
+        height: 10px;
+        -webkit-animation: spin 2s linear infinite;
+        animation: spin 2s linear infinite;
+        display: inline-block;
+        margin-right: 5px;
+    }
+
+    @-webkit-keyframes spin {
+        0% { -webkit-transform: rotate(0deg); }
+        100% { -webkit-transform: rotate(360deg); }
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+</style>
