b2dea69031bd97fc15a43c2cc45e4b40b6fa1b33
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-12 23:43:25 +0300

    icon-element-added

--- src/lib/ui/Elements/Icon/Icon.svelte
+++ src/lib/ui/Elements/Icon/Icon.svelte
@@ -1,1 +1,28 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { createDomElement } from "$lib/utils";
 
+    interface Props {
+        name: string;
+        [attribute: string]: string;
+    }
+
+    let {
+        name,
+        ...attributes
+    }: Props = $props();
+
+    let placeholder: HTMLElement;
+    onMount(async () => {
+        const svgContent = (await import(`$lib/assets/${name}.svg?raw`)).default;
+        const svgElement = createDomElement(svgContent);
+        if (svgElement) {
+            Object.entries(attributes).forEach(([key, value]) => {
+                svgElement.setAttribute(key, value);
+            });
+        }
+        placeholder.replaceWith(svgElement);
+    });
+</script>
+
+<div bind:this={placeholder}></div>
