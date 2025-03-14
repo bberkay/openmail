7124ca8f05e72fd32a7924cf679141c3baf09c33
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-15 02:27:43 +0300

    label added

---
+++ src/lib/ui/Elements/Label/Label.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import { combine } from "$lib/utils";
+    import { type Snippet } from "svelte";
+
+    interface Props {
+        children: Snippet
+        [attribute: string]: unknown;
+    }
+
+    let {
+        children,
+        ...attributes
+    }: Props  = $props();
+
+    const {
+        class: additionalClass,
+        ...restAttributes
+    } = attributes;
+</script>
+
+<label
+    class={combine("label", additionalClass)}
+    {...restAttributes}
+>
+    {@render children()}
+</label>
+
+<style>
+    :global {
+        .label{
+            text-align: left;
+            color: var(--color-text-secondary);
+            font-size: var(--font-size-sm);
+            margin-bottom: var(--spacing-2xs);
+            margin-left: calc(var(--spacing-2xs) / 2);
+
+            &.label-sm {
+                font-size: var(--font-size-xs);
+            }
+        }
+    }
+</style>
