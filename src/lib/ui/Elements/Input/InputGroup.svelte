c53b3d7fed6f7154c0068fa7c138953ea2aa10d2
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-15 02:27:35 +0300

    input-group added with-button mproved

---
+++ src/lib/ui/Elements/Input/InputGroup.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import { type Snippet } from "svelte";
+    import { combine } from "$lib/utils";
+
+    interface Props {
+        direction?: "vertical" | "horizontal"
+        children: Snippet
+        [attribute: string]: unknown;
+    }
+
+    let {
+        direction = "vertical",
+        children,
+        ...attributes
+    }: Props  = $props();
+
+    const {
+        class: additionalClass,
+        ...restAttributes
+    } = attributes;
+</script>
+
+<tr
+    class={combine(
+        `input-group-${direction}`,
+        additionalClass
+    )}
+    {...restAttributes}
+>
+    {@render children()}
+</tr>
+
+<style>
+    :global {
+        .input-group-vertical {
+            display: flex;
+            flex-direction: column;
+            text-align: left;
+            margin-bottom: var(--spacing-xs);
+        }
+
+        .input-group-horizontal {
+            display: flex;
+            align-items: center;
+            gap: var(--spacing-lg);
+
+            & input[type="checkbox"] + label {
+                margin-top: 7px;
+            }
+        }
+    }
+</style>
--- src/lib/ui/Elements/Input/WithButton.svelte
+++ src/lib/ui/Elements/Input/WithButton.svelte
@@ -24,5 +24,60 @@
 </div>
 
 <style>
+    .input-with-button{
+        position: relative;
+        transition: all var(--transition-fast) var(--ease-default);
+        border-bottom: 1px solid var(--color-border);
 
+        & input {
+            border: none !important;
+        }
+
+        & .input-icon + input {
+            margin-left: 40px;
+        }
+
+        &:has(input:focus) {
+            border-color: var(--color-text-primary);
+        }
+
+        & input:focus + .input-icon svg,
+        & input:focus + .input-toggle svg,
+        &:has(input:focus) svg {
+            fill: var(--color-text-primary) !important;
+        }
+
+        & input:focus + .input-toggle {
+            color: var(--color-text-primary) !important;
+        }
+
+        & .input-icon,
+        & .input-toggle {
+            position: absolute;
+            transform: translateY(-50%);
+            cursor: pointer;
+            color: var(--color-text-secondary);
+            background: transparent;
+            border: none;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            transition: all var(--transition-fast) var(--ease-default);
+
+            & svg {
+                width: 20px;
+                height: 20px;
+            }
+        }
+
+        & .input-icon {
+            top: 50%;
+            left: var(--spacing-xs);
+        }
+
+        & .input-toggle {
+            right: var(--spacing-xs);
+            top: 50%;
+        }
+    }
 </style>
