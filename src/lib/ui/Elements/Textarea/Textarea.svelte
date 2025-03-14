99388a9cd276fd92fd562e8b3be1a3615468b6b4
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-15 02:28:16 +0300

    textarea added

---
+++ src/lib/ui/Elements/Textarea/Textarea.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import { combine } from "$lib/utils";
+
+    interface Props {
+        content: string;
+        [attribute: string]: unknown;
+    }
+
+    let {
+        content,
+        ...attributes
+    }: Props  = $props();
+
+    const {
+        class: additionalClass,
+        ...restAttributes
+    } = attributes;
+</script>
+
+<textarea
+    class={combine("textarea", additionalClass)}
+    {...restAttributes}
+>
+    {content}
+</textarea>
+
+<style>
+    :global {
+        .textarea {
+            background-color: transparent;
+            border-radius: 0px;
+            border: none;
+            border-bottom: 1px solid var(--color-border);
+            margin-top: var(--spacing-2xs);
+            transition: all var(--transition-fast) var(--ease-default);
+            color: var(--color-text-primary);
+
+            &:focus {
+                outline: none;
+                border-bottom: 1px solid var(--color-text-primary);
+            }
+        }
+    }
+</style>
