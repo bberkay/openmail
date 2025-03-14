c60eb06ea6006327490861bb9af038b20dddf0fe
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-15 02:27:51 +0300

    link added

---
+++ src/lib/ui/Elements/Link/Link.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { type Snippet } from "svelte";
+    import { combine } from "$lib/utils";
+
+    interface Props {
+        children: Snippet
+        [attribute: string]: unknown;
+    }
+
+    let {
+        children,
+        ...attributes
+    }: Props  = $props();
+
+    const {
+        class: additionalClass,
+        ...restAttributes
+    } = attributes;
+</script>
+
+<a
+    class={combine("link", additionalClass)}
+    {...restAttributes}
+>
+    {@render children()}
+</a>
+
+<style>
+    :global {
+        .link{
+            color: var(--color-link);
+            transition: opacity var(--transition-fast) var(--ease-default);
+
+            &:hover {
+                opacity: 0.8;
+            }
+        }
+    }
+</style>
