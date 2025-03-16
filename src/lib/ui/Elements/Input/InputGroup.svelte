2efdaa75f72ec0f074ff227ed6af06db479db19e
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-16 04:59:59 +0300

    input-group name changed

--- src/lib/ui/Elements/Input/InputGroup.svelte
+++ src/lib/ui/Elements/Input/InputGroup.svelte
@@ -1,51 +1,90 @@
 <script lang="ts">
-    import { type Snippet } from "svelte";
-    import { combine } from "$lib/utils";
+    import { onMount, type Snippet } from "svelte";
+    import { createDomElement } from "$lib/utils";
 
     interface Props {
-        direction?: "vertical" | "horizontal"
         children: Snippet
-        [attribute: string]: unknown;
     }
 
-    let {
-        direction = "vertical",
-        children,
-        ...attributes
-    }: Props  = $props();
+    let { children }: Props  = $props();
 
-    const {
-        class: additionalClass,
-        ...restAttributes
-    } = attributes;
+    let wrapper: HTMLElement;
+    onMount(() => {
+        const preInput = createDomElement('<div class="pre-input"></div>');
+        const postInput = createDomElement('<div class="post-input"></div>');
+
+        let inputFound = false;
+        Array.from(wrapper.children).forEach((child) => {
+            if (child.tagName == "INPUT") {
+                inputFound = true;
+                return
+            }
+
+            if (child.tagName == "BUTTON") {
+                if (!child.classList.contains("btn-inline")){
+                    child.classList.add("btn-inline");
+                }
+            }
+
+            if (!inputFound) {
+                preInput.append(child);
+            } else {
+                postInput.append(child);
+            }
+        });
+
+        wrapper.prepend(preInput);
+        wrapper.append(postInput);
+    })
 </script>
 
-<div
-    class={combine(
-        `input-group-${direction}`,
-        additionalClass
-    )}
-    {...restAttributes}
->
+<div class="input-group" bind:this={wrapper}>
     {@render children()}
 </div>
 
 <style>
     :global {
-        .input-group-vertical {
-            display: flex;
-            flex-direction: column;
-            text-align: left;
-            margin-bottom: var(--spacing-xs);
-        }
-
-        .input-group-horizontal {
+        .input-group{
             display: flex;
             align-items: center;
-            gap: var(--spacing-lg);
+            transition: all var(--transition-fast) var(--ease-default);
+            border-bottom: 1px solid var(--color-border);
 
-            & input[type="checkbox"] + label {
-                margin-top: 7px;
+            & input {
+                border: none !important;
+                flex-grow: 1;
+            }
+
+            &:has(input:focus) {
+                border-color: var(--color-text-primary);
+
+                & svg {
+                    fill: var(--color-text-primary)!important;
+                }
+            }
+
+            & .pre-input,
+            & .post-input {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: center;
+                gap: 10px;
+                cursor: pointer;
+                color: var(--color-text-secondary);
+                background: transparent;
+                border: none;
+                transition: all var(--transition-fast) var(--ease-default);
+
+                & svg {
+                    width: var(--font-size-lg);
+                    height: var(--font-size-lg);
+                }
+            }
+
+            & .pre-input + input,
+            & input + .post-input {
+                margin-left: var(--spacing-2xs);
             }
         }
     }
