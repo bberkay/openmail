fd98f947ff0034dc1e3593c4e4a657ca6e49e5d3
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-17 04:42:37 +0300

    isLoading moved to Register

--- src/lib/ui/Layout/Landing/Register.svelte
+++ src/lib/ui/Layout/Landing/Register.svelte
@@ -4,18 +4,21 @@
     import AccountList from "$lib/ui/Layout/Landing/Register/AccountList.svelte";
     import AddAccountForm from "$lib/ui/Layout/Landing/Register/AddAccountForm.svelte";
     import EditAccountForm from "$lib/ui/Layout/Landing/Register/EditAccountForm.svelte";
+    import Loading from "$lib/ui/Layout/Landing/Loading.svelte";
 
+    let isLoading: boolean = $derived(SharedStore.server === "");
     let isEditingAccount: Account | null = $state(null);
     let isListingAccount: boolean = $state(SharedStore.failedAccounts.length > 0);
 </script>
 
-{#if isEditingAccount}
-    <EditAccountForm bind:isEditingAccount/>
-{:else if isListingAccount}
-    <AccountList
-        bind:isEditingAccount
-        bind:isListingAccount
-    />
+{#if isLoading}
+    <Loading />
 {:else}
-    <AddAccountForm bind:isListingAccount/>
+    {#if isEditingAccount}
+        <EditAccountForm bind:isEditingAccount />
+    {:else if isListingAccount}
+        <AccountList bind:isEditingAccount bind:isListingAccount />
+    {:else}
+        <AddAccountForm bind:isListingAccount />
+    {/if}
 {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,22 @@
 <script lang="ts">
     import { SharedStore } from "$lib/stores/shared.svelte";
     import Main from "$lib/ui/Layout/Main.svelte";
+    import Navbar from "$lib/ui/Layout/Main/Navbar.svelte";
     import Content from "$lib/ui/Layout/Main/Content.svelte";
     import Inbox from "$lib/ui/Layout/Main/Content/Inbox.svelte";
-    import Account from "$lib/ui/Layout/Main/Navbar/Account.svelte";
-    import SearchBar from "$lib/ui/Layout/Main/Navbar/SearchBar.svelte";
-    import Sidebar from "$lib/ui/Layout/Main/Sidebar.svelte";
-    import FolderMenu from "$lib/ui/Layout/Main/Sidebar/FolderMenu.svelte";
     import Landing from "$lib/ui/Layout/Landing.svelte";
     import Register from "$lib/ui/Layout/Landing/Register.svelte";
-    import Loading from "$lib/ui/Layout/Landing/Loading.svelte";
-
-    let isLoading: boolean = $derived(SharedStore.server === "");
 </script>
 
 {#if SharedStore.mailboxes.length > 0}
     <Main>
-        <div class="mailbox">
-            <section style="width:20%;margin-right:5px;">
-                <Sidebar>
-                    <Account />
-                    <FolderMenu />
-                </Sidebar>
-            </section>
-            <section style="width:80%;">
-                <SearchBar />
-                <div class="card">
-                    <Content>
-                        <Inbox />
-                    </Content>
-                </div>
-            </section>
-        </div>
+        <Navbar />
+        <Content>
+            <Inbox />
+        </Content>
     </Main>
 {:else}
     <Landing>
-        {#if isLoading}
-            <Loading />
-        {:else}
-            <Register />
-        {/if}
+        <Register />
     </Landing>
 {/if}
-
-<style>
-    .mailbox {
-        display: flex;
-    }
-</style>
