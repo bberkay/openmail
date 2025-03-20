1d2e6804de13bf0a489ce4e60c06243f5284eb91
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-20 21:56:37 +0300

    navbar account finished

--- src/lib/ui/Layout/Main/Navbar/Account.svelte
+++ src/lib/ui/Layout/Main/Navbar/Account.svelte
@@ -1,36 +1,24 @@
 <script lang="ts">
     import { SharedStore } from "$lib/stores/shared.svelte";
-    import { MailboxController } from "$lib/controllers/MailboxController";
     import { Folder, type Account } from "$lib/types";
     import * as Select from "$lib/ui/Components/Select";
     import { createSenderAddress } from "$lib/utils";
-    import { show as showMessage } from "$lib/ui/Components/Message";
 
-    const mailboxController = new MailboxController();
+    const setCurrentAccount = async (selectedAccountEmailAddr: string) => {
+        const selectedAccount = SharedStore.accounts.find(
+            (account: Account) => account.email_address === selectedAccountEmailAddr
+        )!;
 
-    const handleAccount = (selectedAccountEmailAddr: string | null) => {
-        if (selectedAccountEmailAddr) {
-            const selectedAccount = SharedStore.accounts.find(
-                (account: Account) => account.email_address === selectedAccountEmailAddr
-            );
-            if (!selectedAccount) {
-                showMessage({content: "Selected account could not found"});
-                return;
-            }
-
-            SharedStore.currentAccount = selectedAccount || null;
-            SharedStore.currentFolder = Folder.Inbox;
-            mailboxController.init(SharedStore.currentAccount);
-        }
+        SharedStore.currentAccount = selectedAccount;
+        SharedStore.currentFolder = Folder.Inbox;
     }
 </script>
 
 <Select.Root
-    onchange={handleAccount}
-    value={SharedStore.currentAccount?.email_address}
+    onchange={setCurrentAccount}
+    value={SharedStore.currentAccount!.email_address}
     placeholder="Account"
 >
-    <Select.Option value="">Home</Select.Option>
     {#each SharedStore.accounts as account}
         <Select.Option value={account.email_address}>
             {createSenderAddress(account.email_address, account.fullname)}
