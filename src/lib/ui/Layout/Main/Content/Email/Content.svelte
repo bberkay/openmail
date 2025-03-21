a264b3a515e72f48add83b13fdff56c714c239aa
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-21 23:01:40 +0300

    content-separated-from-email-completelty

---
+++ src/lib/ui/Layout/Main/Content/Email/Content.svelte
@@ -0,0 +1,141 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { SharedStore } from "$lib/stores/shared.svelte";
+    import { MailboxController } from "$lib/controllers/MailboxController";
+    import { create, BaseDirectory } from '@tauri-apps/plugin-fs';
+    import { ATTACHMENT_TEMPLATE, SENDER_TO_RECEIVER_TEMPLATE } from '$lib/constants';
+    import { type Account, type Email } from "$lib/types";
+    import { extractEmailAddress, extractFullname, makeSizeHumanReadable } from "$lib/utils";
+    import * as Button from "$lib/ui/Components/Button";
+    import Badge from "$lib/ui/Components/Badge";
+    import { show as showMessage } from "$lib/ui/Components/Message";
+
+    const mailboxController = new MailboxController();
+
+    interface Props {
+        account: Account;
+        email: Email;
+    }
+
+    let {
+        account,
+        email
+    }: Props = $props();
+
+    let currentMailbox = $derived(SharedStore.mailboxes.find(
+        task => task.result.folder === SharedStore.currentFolder
+    )!.result);
+
+    let body: HTMLElement;
+    onMount(() => { renderBody() });
+
+    function renderBody(): void {
+        body.innerHTML = "";
+
+        let iframe = document.createElement("iframe");
+        body.appendChild(iframe);
+
+        let iframeDoc: Document | null;
+        iframeDoc = iframe.contentWindow
+            ? iframe.contentWindow.document
+            : iframe.contentDocument;
+        if (iframeDoc) {
+            iframeDoc.open();
+            iframeDoc.writeln(email.body);
+            iframeDoc.close();
+
+            body.style.height = iframeDoc.body.scrollHeight + "px";
+        }
+    }
+
+    const downloadAttachment = async (index: number) => {
+        const attachment = email.attachments![index];
+        const response = await mailboxController.downloadAttachment(
+            account,
+            SharedStore.currentFolder!,
+            email.uid,
+            attachment.name,
+            attachment.cid || undefined
+        );
+
+        if (!response.success || !response.data) {
+            showMessage({content: "Error, attachment could not be downloaded properly."});
+            console.error(response.message);
+            return;
+        }
+
+        const file = await create(response.data.name, { baseDir: BaseDirectory.Download });
+        await file.write(Uint8Array.from(atob(response.data.data), (char) => char.charCodeAt(0)));
+        await file.close();
+    }
+</script>
+
+<div class="email-content">
+    <div class="flags">
+        {#if Object.hasOwn(email, "flags") && email.flags}
+            {#each email.flags as flag}
+                <Badge content={flag} />
+            {/each}
+        {/if}
+    </div>
+    <div class="subject">
+        {email.subject || ""}
+    </div>
+    <div class="sender-to-receiver">
+        {
+            SENDER_TO_RECEIVER_TEMPLATE
+                .replace("{sender_fullname}", extractFullname(email.sender))
+                .replace("{sender_email}", extractEmailAddress(email.sender))
+                .replace("{sent_at}", email.date)
+                .trim()
+        }
+    </div>
+    <div class="separator"></div>
+    <div class="body" bind:this={body}>
+        <!-- Body is going to be here -->
+    </div>
+    {#if Object.hasOwn(email, "attachments") && email.attachments}
+        <div class="separator"></div>
+        <div id="attachments">
+            {#each email.attachments as attachment, index}
+                <Button.Action
+                    class="btn-outline"
+                    download={attachment.name}
+                    onclick={() => downloadAttachment(index)}
+                >
+                    {
+                        ATTACHMENT_TEMPLATE
+                            .replace("{attachment_name}", attachment.name)
+                            .replace("{attachment_size}", makeSizeHumanReadable(parseInt(attachment.size)))
+                            .trim()
+                    }
+                </Button.Action>
+            {/each}
+        </div>
+    {/if}
+</div>
+
+<style>
+    :global {
+        .email-content {
+            display: flex;
+            flex-direction: column;
+            padding: var(--spacing-lg);
+            border: 1px solid var(--color-border-subtle);
+            border-radius: var(--radius-sm);
+
+            & .subject {
+                margin-top: var(--spacing-md);
+                display: flex;
+                flex-direction: column;
+                font-size: var(--font-size-lg);
+            }
+
+            & .sender-to-receiver {
+                margin-top: var(--spacing-xs);
+                color: var(--color-text-secondary);
+                font-size: var(--font-size-sm);
+            }
+        }
+    }
+</style>
