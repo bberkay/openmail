943ef48cd3c94e61096be8b627f161f9d194a7b0
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-22 22:17:42 +0300

    rename folder feature added

---
+++ src/lib/ui/Layout/Main/Navbar/Folders/RenameFolder.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+    import { SharedStore } from "$lib/stores/shared.svelte";
+    import { MailboxController } from "$lib/controllers/MailboxController";
+    import Form, { FormGroup } from "$lib/ui/Components/Form";
+    import Modal from "$lib/ui/Components/Modal";
+    import * as Input from "$lib/ui/Components/Input";
+    import * as Button from "$lib/ui/Components/Button";
+    import Label from "$lib/ui/Components/Label";
+    import { show as showMessage } from "$lib/ui/Components/Message";
+
+    interface Props {
+        folderName: string;
+    }
+
+    let { folderName }: Props = $props();
+
+    const handleRenameFolderForm = async (e: Event): Promise<void> => {
+        const target = e.target as HTMLFormElement;
+        const folderPath = target.querySelector<HTMLInputElement>(
+            'input[name="folder_name"]',
+        )!.value;
+        let newFolderName = target.querySelector<HTMLSelectElement>(
+            'input[name="new_folder_name"]',
+        )!.value;
+
+        const response = await MailboxController.renameFolder(
+            SharedStore.currentAccount,
+            folderPath,
+            newFolderName
+        );
+        if (!response.success) {
+            showMessage({ content: "Error while renaming folder." });
+            console.error(response.message);
+        }
+
+        target.reset();
+    };
+</script>
+
+<Modal>
+    <Form onsubmit={handleRenameFolderForm}>
+        <div>
+            <FormGroup>
+                <Label for="folder-name">Folder Name</Label>
+                <Input.Basic
+                    type="text"
+                    name="folder_name"
+                    id="folder-name"
+                    value={folderName}
+                    disabled
+                    required
+                />
+            </FormGroup>
+            <FormGroup>
+                <Label for="new-folder-name">New Folder Name</Label>
+                <Input.Basic
+                    type="text"
+                    name="new_folder_name"
+                    id="new-folder-name"
+                    placeholder="My Custom Folder"
+                    required
+                />
+            </FormGroup>
+            <Button.Basic type="submit">Rename</Button.Basic>
+        </div>
+    </Form>
+</Modal>
