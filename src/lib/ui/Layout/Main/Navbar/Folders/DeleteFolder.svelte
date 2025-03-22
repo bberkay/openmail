d978f1187f845ae0fc356fa796610df3c533bc3c
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-22 22:04:15 +0300

    delete folder feature added

---
+++ src/lib/ui/Layout/Main/Navbar/Folders/DeleteFolder.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    import { SharedStore } from "$lib/stores/shared.svelte";
+    import { MailboxController } from "$lib/controllers/MailboxController";
+    import Form, { FormGroup } from "$lib/ui/Components/Form";
+    import Modal from "$lib/ui/Components/Modal";
+    import * as Input from "$lib/ui/Components/Input";
+    import * as Button from "$lib/ui/Components/Button";
+    import Label from "$lib/ui/Components/Label";
+    import { show as showMessage } from "$lib/ui/Components/Message";
+
+    interface Props {
+        folderName: string;
+    }
+
+    let { folderName }: Props = $props();
+
+    const handleDeleteFolderForm = async (e: Event): Promise<void> => {
+        const target = e.target as HTMLFormElement;
+        const folderName = target.querySelector<HTMLInputElement>(
+            'input[name="folder_name"]',
+        )!.value;
+        const subfolders = target.querySelector<HTMLInputElement>(
+            'input[name="subfolders"]',
+        )!.checked;
+
+        const response = await MailboxController.deleteFolder(
+            SharedStore.currentAccount,
+            folderName,
+            subfolders,
+        );
+        if (!response.success) {
+            showMessage({ content: "Error while deleting folder." });
+            console.error(response.message);
+        }
+
+        target.reset();
+    };
+</script>
+
+<Modal>
+    <Form onsubmit={handleDeleteFolderForm}>
+        <div>
+            <FormGroup>
+                <Label for="folder-name">Folder Name</Label>
+                <Input.Basic
+                    type="text"
+                    name="folder_name"
+                    id="folder-name"
+                    value={folderName}
+                    disabled
+                    required
+                />
+            </FormGroup>
+            <FormGroup>
+                <label for="subfolders">Delete Subfolders</label>
+                <Input.Basic
+                    type="checkbox"
+                    name="subfolders"
+                    id="subfolders"
+                    required
+                />
+            </FormGroup>
+            <Button.Basic type="submit">Delete</Button.Basic>
+        </div>
+    </Form>
+</Modal>
