bd148d9de6b639b9641b649a3cf04cb553ecef7b
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-22 20:52:59 +0300

    select separator added

---
+++ src/lib/ui/Components/Select/Separator.svelte
@@ -0,0 +1,12 @@
+<div class="separator"></div>
+
+<style>
+    :global {
+        .separator {
+            width: 100%;
+            margin-bottom: var(--spacing-2xs);
+            background-color: var(--color-border);
+            height: 1px;
+        }
+    }
+</style>
--- src/lib/ui/Components/Select/index.ts
+++ src/lib/ui/Components/Select/index.ts
@@ -1,7 +1,9 @@
 import Select from './Select.svelte';
 import Option from './Option.svelte';
+import Separator from './Separator.svelte';
 
 export {
   Select as Root,
   Option,
+  Separator
 };
