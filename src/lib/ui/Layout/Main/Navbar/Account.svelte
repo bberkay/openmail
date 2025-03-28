5e4df873c0622075dcf05525032cfb4d14146db3
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-28 21:40:31 +0300

    list inbox after account selection

--- src/lib/ui/Layout/Main/Navbar/Account.svelte
+++ src/lib/ui/Layout/Main/Navbar/Account.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import { SharedStore } from "$lib/stores/shared.svelte";
+    import { MailboxController } from "$lib/controllers/MailboxController";
     import { Folder, type Account } from "$lib/types";
     import * as Select from "$lib/ui/Components/Select";
-    import { createSenderAddress } from "$lib/utils";
+    import { createSenderAddress, isStandardFolder } from "$lib/utils";
     import Inbox from "$lib/ui/Layout/Main/Content/Inbox.svelte";
     import { showThis as showContent } from "$lib/ui/Layout/Main/Content.svelte";
+    import { show as showMessage } from "$lib/ui/Components/Message";
 
     const AccountOperation = {
         Home: "create",
@@ -14,17 +16,62 @@
         Quit: "quit",
     } as const;
 
-    const setCurrentAccount = (selectedAccountEmailAddr: string) => {
-        const selectedAccount = SharedStore.accounts.find(
-            (account: Account) =>
-                account.email_address === selectedAccountEmailAddr,
-        )!;
+    const setCurrentAccount = async (
+        emailAddressOrHome: "home" | string,
+    ): Promise<void> => {
+        SharedStore.currentFolder = Folder.Inbox;
+        SharedStore.currentAccount =
+            emailAddressOrHome === "home"
+                ? emailAddressOrHome
+                : SharedStore.accounts.find(
+                      (account: Account) =>
+                          account.email_address === emailAddressOrHome,
+                  )!;
 
-        SharedStore.currentAccount = selectedAccount;
-    };
+        const nonInboxAccounts: Account[] = [];
+        const mailboxesToCheck = emailAddressOrHome === "home"
+            ? SharedStore.mailboxes
+            : [emailAddressOrHome]
+        for (const emailAddr in mailboxesToCheck) {
+            if (
+                isStandardFolder(SharedStore.mailboxes[emailAddr].folder, Folder.Inbox)
+            ) {
+                nonInboxAccounts.push(
+                    SharedStore.accounts.find(
+                        (acc) => acc.email_address === emailAddr,
+                    )!,
+                );
+            }
+        }
 
-    const showHome = () => {
-        SharedStore.currentAccount = "home";
+        if (nonInboxAccounts.length >= 1) {
+            const response = await MailboxController.getMailboxes(
+                nonInboxAccounts,
+                Folder.Inbox,
+            );
+            if (!response.success) {
+                showMessage({
+                    content: "Error, account could not set.",
+                });
+                console.error(response.message);
+                return;
+            }
+        }
+
+        if (emailAddressOrHome === "home") {
+            SharedStore.currentMailbox.folder = Folder.Inbox;
+            Object.values(SharedStore.mailboxes).forEach((mailbox) => {
+                SharedStore.currentMailbox.total += mailbox.total;
+                SharedStore.currentMailbox.emails.prev.push(...mailbox.emails.prev);
+                SharedStore.currentMailbox.emails.current.push(...mailbox.emails.current);
+                SharedStore.currentMailbox.emails.next.push(...mailbox.emails.next);
+            });
+            Object.values(SharedStore.currentMailbox.emails).forEach((emails) => {
+                emails.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+            })
+        } else {
+            SharedStore.currentMailbox = SharedStore.mailboxes[(SharedStore.currentAccount as Account).email_address];
+        }
         showContent(Inbox);
     };
 
@@ -38,9 +85,6 @@
 
     const handleOperation = (selectedOperation: string) => {
         switch (selectedOperation) {
-            case AccountOperation.Home:
-                showHome();
-                break;
             case AccountOperation.Minimize:
                 minimize();
                 break;
@@ -54,8 +98,6 @@
                 quit();
                 break;
             default:
-                // if selectedOperation is none of the above, then it
-                // must be an email address.
                 setCurrentAccount(selectedOperation);
                 break;
         }
