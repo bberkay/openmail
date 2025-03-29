fd4429a7bdcaa05c28c28d6e7c60db86bc170ffd
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-03-30 01:06:00 +0300

    currentMailbox removed

--- src/lib/stores/shared.svelte.ts
+++ src/lib/stores/shared.svelte.ts
@@ -9,34 +9,31 @@ export enum SharedStoreKeys {
     server = "server",
     accounts = "accounts",
     failedAccounts = "failedAccounts",
-    mailboxes = "mailboxes",
-    recentEmails = "recentEmails",
-    standardFolders = "standardFolders",
-    customFolders = "customFolders",
     currentAccount = "currentAccount",
-    currentMailbox = "currentMailbox",
+    recentEmails = "recentEmails",
+    mailboxes = "mailboxes",
+    folders = "folders",
 }
 
 interface ISharedStore {
     [SharedStoreKeys.server]: string;
     [SharedStoreKeys.accounts]: Account[];
     [SharedStoreKeys.failedAccounts]: Account[];
-    [SharedStoreKeys.recentEmails]: OpenMailTaskResults<Email[]>;
-    [SharedStoreKeys.mailboxes]: OpenMailTaskResults<Mailbox>
-    [SharedStoreKeys.standardFolders]: OpenMailTaskResults<string[]>;
-    [SharedStoreKeys.customFolders]: OpenMailTaskResults<string[]>;
     [SharedStoreKeys.currentAccount]: "home" | Account;
-    [SharedStoreKeys.currentMailbox]: Mailbox
+    [SharedStoreKeys.recentEmails]: OpenMailTaskResults<Email[]>;
+    [SharedStoreKeys.mailboxes]: OpenMailTaskResults<Mailbox>;
+    [SharedStoreKeys.folders]: OpenMailTaskResults<{
+        standard: string[],
+        custom: string[]
+    }>;
 }
 
 export let SharedStore: { [K in SharedStoreKeys]: ISharedStore[K] } = $state({
     [SharedStoreKeys.server]: "",
     [SharedStoreKeys.accounts]: [],
     [SharedStoreKeys.failedAccounts]: [],
+    [SharedStoreKeys.currentAccount]: "home",
     [SharedStoreKeys.recentEmails]: {},
     [SharedStoreKeys.mailboxes]: {},
-    [SharedStoreKeys.standardFolders]: {},
-    [SharedStoreKeys.customFolders]: {},
-    [SharedStoreKeys.currentAccount]: "home",
-    [SharedStoreKeys.currentMailbox]: { folder: "", emails: { prev: [], current: [], next: [] } , total: 0},
+    [SharedStoreKeys.folders]: {},
 });
