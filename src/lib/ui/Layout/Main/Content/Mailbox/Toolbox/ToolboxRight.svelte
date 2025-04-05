a87a840d2d1d9ec5842611032b851d066e518586
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-04-05 19:04:09 +0300

    Update ToolboxRight.svelte

--- src/lib/ui/Layout/Main/Content/Mailbox/Toolbox/ToolboxRight.svelte
+++ src/lib/ui/Layout/Main/Content/Mailbox/Toolbox/ToolboxRight.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     import { SharedStore } from "$lib/stores/shared.svelte";
     import { type Mailbox } from "$lib/types";
-    import { MailboxController, MAILBOX_LENGTH } from "$lib/controllers/MailboxController";
     import {
-        MAILBOX_PAGINATION_TEMPLATE,
-    } from "$lib/constants";
+        MailboxController,
+        MAILBOX_LENGTH,
+    } from "$lib/controllers/MailboxController";
+    import { MAILBOX_PAGINATION_TEMPLATE } from "$lib/constants";
     import * as Button from "$lib/ui/Components/Button";
 
     interface Props {
@@ -20,25 +21,35 @@
     const getPreviousEmails = async (): Promise<void> => {
         if (currentOffset <= MAILBOX_LENGTH) return;
         if (!waitPrev) {
-            const emailAddrs = SharedStore.currentAccount !== "home"
-                ? [SharedStore.currentAccount.email_address]
-                : SharedStore.accounts.map(acc => acc.email_address);
+            const emailAddrs =
+                SharedStore.currentAccount !== "home"
+                    ? [SharedStore.currentAccount.email_address]
+                    : SharedStore.accounts.map((acc) => acc.email_address);
 
             waitPrev = setInterval(() => {
                 if (currentMailbox.emails.prev.length >= MAILBOX_LENGTH) {
+                    // TODO: Description
                     currentMailbox.emails.next = currentMailbox.emails.current;
                     currentMailbox.emails.current = currentMailbox.emails.prev;
                     currentMailbox.emails.prev = [];
-                    const offsetStart = Math.max(1, currentOffset - MAILBOX_LENGTH * 2);
-                    const offsetEnd = Math.max(MAILBOX_LENGTH, currentOffset - 1 - MAILBOX_LENGTH);
-                    if (offsetEnd < currentOffset) {
-                        emailAddrs.forEach(emailAddr => {
+                    const prevOffsetStart = Math.max(
+                        1,
+                        currentOffset - MAILBOX_LENGTH * 2,
+                    );
+                    const prevOffsetEnd = Math.max(
+                        MAILBOX_LENGTH,
+                        currentOffset - 1 - MAILBOX_LENGTH,
+                    );
+                    if (prevOffsetEnd < currentOffset) {
+                        emailAddrs.forEach((emailAddr) => {
                             MailboxController.paginateEmails(
-                                SharedStore.accounts.find(acc => acc.email_address === emailAddr)!,
-                                offsetStart,
-                                offsetEnd
+                                SharedStore.accounts.find(
+                                    (acc) => acc.email_address === emailAddr,
+                                )!,
+                                prevOffsetStart,
+                                prevOffsetEnd,
                             );
-                        })
+                        });
                     }
                     currentOffset = Math.max(1, currentOffset - MAILBOX_LENGTH);
                     clearInterval(waitPrev!);
@@ -51,35 +62,44 @@
     const getNextEmails = async (): Promise<void> => {
         if (currentOffset >= currentMailbox.total) return;
         if (!waitNext) {
-            const emailAddrs = SharedStore.currentAccount !== "home"
-                ? [SharedStore.currentAccount.email_address]
-                : SharedStore.accounts.filter((acc) => {
-                    return SharedStore.mailboxes[acc.email_address].total > currentOffset;
-                });
+            const emailAddrs =
+                SharedStore.currentAccount !== "home"
+                    ? [SharedStore.currentAccount.email_address]
+                    : SharedStore.accounts.filter((acc) => {
+                          return (
+                              SharedStore.mailboxes[acc.email_address].total >
+                              currentOffset
+                          );
+                      });
 
             waitNext = setInterval(() => {
                 if (currentMailbox.emails.next.length >= MAILBOX_LENGTH) {
                     currentMailbox.emails.prev = currentMailbox.emails.current;
                     currentMailbox.emails.current = currentMailbox.emails.next;
                     currentMailbox.emails.next = [];
-                    const offsetStart = Math.min(
+                    const nextOffsetStart = Math.min(
                         currentMailbox.total,
                         Math.max(1, currentOffset + MAILBOX_LENGTH * 2),
                     );
-                    const offsetEnd = Math.min(
+                    const nextOffsetEnd = Math.min(
                         currentMailbox.total,
-                        Math.max(1, offsetStart - 1 + MAILBOX_LENGTH),
+                        Math.max(1, nextOffsetStart - 1 + MAILBOX_LENGTH),
                     );
-                    if (offsetStart <= currentMailbox.total) {
-                        emailAddrs.forEach(emailAddr => {
+                    if (nextOffsetStart <= currentMailbox.total) {
+                        emailAddrs.forEach((emailAddr) => {
                             MailboxController.paginateEmails(
-                                SharedStore.accounts.find(acc => acc.email_address === emailAddr)!,
-                                offsetStart,
-                                offsetEnd
+                                SharedStore.accounts.find(
+                                    (acc) => acc.email_address === emailAddr,
+                                )!,
+                                nextOffsetStart,
+                                nextOffsetEnd,
                             );
-                        })
+                        });
                     }
-                    currentOffset = Math.min(currentMailbox.total, currentOffset + MAILBOX_LENGTH);
+                    currentOffset = Math.min(
+                        currentMailbox.total,
+                        currentOffset + MAILBOX_LENGTH,
+                    );
                     clearInterval(waitNext!);
                     waitNext = null;
                 }
@@ -106,7 +126,7 @@
             )
                 .replace(
                     "{offset_end}",
-                    (currentOffset  - 1 + MAILBOX_LENGTH).toString(),
+                    (currentOffset - 1 + MAILBOX_LENGTH).toString(),
                 )
                 .replace("{total}", currentMailbox.total.toString())
                 .trim()}
