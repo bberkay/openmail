6d2439e06861eda6cd649e3bc821b133f403f72c
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-04-12 08:12:57 +0300

    Update shared.svelte.ts

--- src/lib/stores/shared.svelte.ts
+++ src/lib/stores/shared.svelte.ts
@@ -8,41 +8,41 @@ import {
 export enum SharedStoreKeys {
     server = "server",
     accounts = "accounts",
-    failedAccounts = "failedAccounts",
-    hierarchyDelimiters = "hierarchyDelimiters",
     currentAccount = "currentAccount",
+    folders = "folders",
+    hierarchyDelimiters = "hierarchyDelimiters",
     recentEmails = "recentEmails",
     mailboxes = "mailboxes",
+    failedAccounts = "failedAccounts",
     failedMailboxes = "failedMailboxes",
-    folders = "folders",
     failedFolders = "failedFolders"
 }
 
 interface ISharedStore {
     [SharedStoreKeys.server]: string;
     [SharedStoreKeys.accounts]: Account[];
-    [SharedStoreKeys.failedAccounts]: Account[];
-    [SharedStoreKeys.hierarchyDelimiters]: OpenMailTaskResults<string>;
     [SharedStoreKeys.currentAccount]: "home" | Account;
-    [SharedStoreKeys.recentEmails]: OpenMailTaskResults<Email[]>;
-    [SharedStoreKeys.mailboxes]: OpenMailTaskResults<Mailbox>;
-    [SharedStoreKeys.failedMailboxes]: Account[]; // Accounts of failed mailboxes
     [SharedStoreKeys.folders]: OpenMailTaskResults<{
         standard: string[],
         custom: string[]
     }>;
+    [SharedStoreKeys.hierarchyDelimiters]: OpenMailTaskResults<string>;
+    [SharedStoreKeys.recentEmails]: OpenMailTaskResults<Email[]>;
+    [SharedStoreKeys.mailboxes]: OpenMailTaskResults<Mailbox>;
+    [SharedStoreKeys.failedAccounts]: Account[];
     [SharedStoreKeys.failedFolders]: Account[]; // Accounts of failed folders
+    [SharedStoreKeys.failedMailboxes]: Account[]; // Accounts of failed mailboxes
 }
 
 export let SharedStore: { [K in SharedStoreKeys]: ISharedStore[K] } = $state({
     [SharedStoreKeys.server]: "",
     [SharedStoreKeys.accounts]: [],
-    [SharedStoreKeys.failedAccounts]: [],
-    [SharedStoreKeys.hierarchyDelimiters]: {},
     [SharedStoreKeys.currentAccount]: "home",
+    [SharedStoreKeys.folders]: {},
+    [SharedStoreKeys.hierarchyDelimiters]: {},
     [SharedStoreKeys.recentEmails]: {},
     [SharedStoreKeys.mailboxes]: {},
-    [SharedStoreKeys.failedMailboxes]: [],
-    [SharedStoreKeys.folders]: {},
+    [SharedStoreKeys.failedAccounts]: [],
     [SharedStoreKeys.failedFolders]: [],
+    [SharedStoreKeys.failedMailboxes]: [],
 });
