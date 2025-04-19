882de2e46efd259faa85c06532071178fec21167
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-04-19 18:24:51 +0300

    connect to local server moved to hooks

---
+++ src/hooks.client.ts
@@ -0,0 +1,50 @@
+import type { Handle, ServerInit } from "@sveltejs/kit";
+import { invoke } from "@tauri-apps/api/core";
+import { TauriCommand } from "$lib/types";
+import { SharedStore } from "$lib/stores/shared.svelte";
+import { ApiService, GetRoutes } from "$lib/services/ApiService";
+import { SERVER_CONNECTION_TRY_SLEEP_MS } from "$lib/constants";
+
+async function loadPreferences(): Promise<void> {
+    const response = await ApiService.get(GetRoutes.GET_PREFERENCES);
+    if (response.success && response.data) {
+        SharedStore.preferences = response.data;
+    }
+}
+
+async function loadServerUrl(): Promise<void> {
+    if (SharedStore.server) {
+        return;
+    }
+
+    const serverUrl = await invoke<string>(TauriCommand.GET_SERVER_URL);
+    const response = await ApiService.get(GetRoutes.HELLO);
+    if (response.success) {
+        SharedStore.server = serverUrl;
+    }
+}
+
+export const init: ServerInit = async () => {
+    const connectToLocalServer = async () => {
+        await loadServerUrl();
+        if (SharedStore.server) {
+            await loadPreferences();
+        } else {
+            setTimeout(async () => {
+                await connectToLocalServer();
+            }, SERVER_CONNECTION_TRY_SLEEP_MS);
+        }
+    }
+
+    await connectToLocalServer();
+}
+
+export const handle: Handle = ({ event, resolve }) => {
+    // TODO: Should we wait for the preferences?
+    return resolve(event, {
+        transformPageChunk: ({ html }) =>
+            html
+                .replace("%lang%", SharedStore.preferences.language)
+                .replace("%data-color-scheme%", SharedStore.preferences.theme),
+    });
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
     import { SharedStore } from "$lib/stores/shared.svelte";
+    import { onMount } from "svelte";
+    import { AccountController } from "$lib/controllers/AccountController";
+    import { show as showMessage } from "$lib/ui/Components/Message";
+    import { DEFAULT_LANGUAGE } from "$lib/constants";
+    import { local } from "$lib/locales";
     import Main from "$lib/ui/Layout/Main.svelte";
     import Navbar from "$lib/ui/Layout/Main/Navbar.svelte";
     import Content from "$lib/ui/Layout/Main/Content.svelte";
     import Mailbox from "$lib/ui/Layout/Main/Content/Mailbox.svelte";
     import Landing from "$lib/ui/Layout/Landing.svelte";
     import Register from "$lib/ui/Layout/Landing/Register.svelte";
+    import Loading from "$lib/ui/Layout/Landing/Register/Loading.svelte";
+    import Welcome from "$lib/ui/Layout/Landing/Register/Welcome.svelte";
+
+    let isServerLoading = $derived(SharedStore.server === "");
+
+    onMount(async () => {
+        const response = await AccountController.init();
+        if(!response.success) {
+            console.error(response.message);
+            showMessage({
+                title: local.error_initialize_accounts[DEFAULT_LANGUAGE],
+            });
+        }
+    })
 </script>
 
 {#if Object.keys(SharedStore.mailboxes).length > 0}
@@ -17,6 +36,12 @@
     </Main>
 {:else}
     <Landing>
-        <Register />
+        <Register>
+            {#if isServerLoading}
+                <Loading/>
+            {:else}
+                <Welcome/>
+            {/if}
+        </Register>
     </Landing>
 {/if}
