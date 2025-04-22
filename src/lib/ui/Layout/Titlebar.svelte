63c47304c7a4ee0970c3d30035e666a925d4d1a4
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-04-22 21:12:23 +0300

    titlebar customization

---
+++ src/lib/ui/Layout/Titlebar.svelte
@@ -0,0 +1,64 @@
+<script>
+    import { getCurrentWindow } from '@tauri-apps/api/window';
+    import { onMount } from "svelte";
+    import Icon from "$lib/ui/Components/Icon";
+
+    const appWindow = getCurrentWindow();
+
+    onMount(() => {
+        document
+          .getElementById('titlebar-minimize')
+          ?.addEventListener('click', () => appWindow.minimize());
+        document
+          .getElementById('titlebar-maximize')
+          ?.addEventListener('click', () => appWindow.toggleMaximize());
+        document
+          .getElementById('titlebar-close')
+          ?.addEventListener('click', () => appWindow.close());
+    });
+</script>
+
+<div data-tauri-drag-region class="titlebar">
+    <div class="titlebar-button" id="titlebar-minimize">
+        <Icon name="minimize" />
+    </div>
+    <div class="titlebar-button" id="titlebar-maximize">
+        <Icon name="maximize" />
+    </div>
+    <div class="titlebar-button" id="titlebar-close">
+        <Icon name="close" />
+    </div>
+</div>
+
+<style>
+    :global {
+        .titlebar {
+            height: var(--titlebar-height);
+            background: var(--color-text-secondary);
+            user-select: none;
+            display: flex;
+            justify-content: flex-end;
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            border:1px solid transparent;
+            border-top-left-radius: var(--radius-sm);
+            border-top-right-radius: var(--radius-sm);
+
+            & .titlebar-button {
+                display: inline-flex;
+                justify-content: center;
+                align-items: center;
+                width: var(--titlebar-height);
+                height: var(--titlebar-height);
+                user-select: none;
+                -webkit-user-select: none;
+
+                &:hover {
+                    background: var(--color-hover);
+                }
+            }
+        }
+    }
+</style>
