f3a18c2e39d3aa482f3946b671527a5760d48625
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-04-29 03:08:17 +0300

    loading page improved

--- src/lib/ui/Components/Loader/Spinner.svelte
+++ src/lib/ui/Components/Loader/Spinner.svelte
@@ -1,33 +1,58 @@
 <script lang="ts">
+    import { combine } from "$lib/utils";
+
     interface Props {
-        text?: string;
+        size?: "small" | "medium" | "large";
         [attribute: string]: unknown;
     }
 
     let {
-        text,
+        size = "medium",
         ...attributes
     }: Props = $props();
+
+
+    const {
+        class: additionalClass,
+        ...restAttributes
+    } = attributes;
 </script>
 
-<span {...attributes}>
-    <small class="loader"></small>
-    {#if text}
-        {text}
-    {/if}
-</span>
+<small class={combine(`spinner ${size}`, additionalClass)} {...restAttributes}></small>
 
 <style>
-    .loader {
+    .spinner {
         border: 2px solid var(--color-border);
         border-radius: 50%;
         border-top: 2px solid var(--color-text-primary);
-        width: 10px;
-        height: 10px;
         -webkit-animation: spin 2s linear infinite;
         animation: spin 2s linear infinite;
         display: inline-block;
         margin-right: var(--spacing-2xs);
+
+        &.small {
+            width: var(--font-size-xs);
+            height: var(--font-size-xs);
+            border-width: 2px;
+            -webkit-animation-duration: 2s;
+            animation-duration: 2s;
+        }
+
+        &.medium {
+            width: var(--font-size-lg);
+            height: var(--font-size-lg);
+            border-width: 4px;
+            -webkit-animation-duration: 1.25s;
+            animation-duration: 1.25s;
+        }
+
+        &.large {
+            width: var(--font-size-2xl);
+            height: var(--font-size-2xl);
+            border-width: 6px;
+            -webkit-animation-duration: 1s;
+            animation-duration: 1s;
+        }
     }
 
     @-webkit-keyframes spin {
--- src/lib/ui/Layout/Loading.svelte
+++ src/lib/ui/Layout/Loading.svelte
@@ -4,7 +4,25 @@
     import { Spinner } from "$lib/ui/Components/Loader";
 </script>
 
-<h3>
-    <Spinner/>
-    {local.connecting_to_accounts[DEFAULT_LANGUAGE]}
-</h3>
+<div class="loading-page">
+    <h3>
+        <Spinner size="medium"/>
+        {local.connecting_to_accounts[DEFAULT_LANGUAGE]}
+    </h3>
+</div>
+
+<style>
+    .loading-page {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+
+        & h3 {
+            display: flex;
+            gap: var(--spacing-sm);
+        }
+    }
+</style>
