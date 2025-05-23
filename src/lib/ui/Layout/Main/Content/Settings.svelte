c9d90d80869ca382705daf28b36eed64bed1c891
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-05-24 00:21:33 +0300

    Update Settings.svelte

--- src/lib/ui/Layout/Main/Content/Settings.svelte
+++ src/lib/ui/Layout/Main/Content/Settings.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
     import Menu from "./Settings/Menu.svelte";
-    import Content from "./Settings/Content.svelte";
+    import Content, { backToDefault as showDefaultSettings } from "./Settings/Content.svelte";
     import General from "./Settings/Content/General.svelte";
     import Form from "$lib/ui/Components/Form";
     import { SharedStore } from "$lib/stores/shared.svelte";
     import { FileSystem } from "$lib/services/FileSystem";
+    import { onMount } from "svelte";
 
     const saveChanges = async (e: Event): Promise<void> => {
         const fileSystem = await FileSystem.getInstance();
         await fileSystem.savePreferences(SharedStore.preferences);
         document.dispatchEvent(new CustomEvent("preferencesSaved"));
     };
+
+    onMount(() => {
+        showDefaultSettings();
+    })
 </script>
 
 <div class="settings">
