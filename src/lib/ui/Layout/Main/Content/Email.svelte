3301ae62adaff332d9232d60e58b8666f019e4ae
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-06-03 13:39:20 +0300

    getMailboxContext removed

--- src/lib/ui/Layout/Main/Content/Email.svelte
+++ src/lib/ui/Layout/Main/Content/Email.svelte
@@ -2,7 +2,7 @@
     import { type Account, type Email } from "$lib/types";
     import Toolbox from "./Email/Toolbox.svelte";
     import Content from "./Email/Content.svelte";
-    import { getMailboxContext } from "$lib/ui/Layout/Main/Content/Mailbox";
+    import { getCurrentMailbox } from "$lib/ui/Layout/Main/Content/Mailbox.svelte";
 
     interface Props {
         account: Account;
@@ -11,12 +11,11 @@
 
     let { account, email }: Props = $props();
 
-    const mailboxContext = getMailboxContext();
-
     let currentOffset = $state(
-        mailboxContext
-            .getCurrentMailbox()
-            .emails.current.findIndex((em) => em.uid === email.uid) + 1,
+        getCurrentMailbox()
+            .emails
+            .current
+            .findIndex((em) => em.uid === email.uid) + 1,
     );
 </script>
 
